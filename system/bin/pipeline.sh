e20d2f76dc0becc5b2152f495ed97eea8a031223
Wolfe <paul.d.wolfe@gmail.com>
2018-06-17 17:38:28 -0400

    Log to file and increase max heap
    Change to log4j config to log to an appending file for obvious reasons.
    
    Hardcode max heap to 64G. Will make this runtime configurable later
    but want to evaluate impact on production tests quickly.

--- system/bin/pipeline.sh
+++ system/bin/pipeline.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/usr/bin/java -jar /usr/share/pipeline2/system.jar
+/usr/bin/java -Xmx64G -jar /usr/share/pipeline2/system.jar
 status=$?
 if [ $status -ne 0 ]; then
   echo "Failed to start pipeline: $status"
