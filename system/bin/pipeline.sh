6e3d398f05cc1094600ea7f96b812be277366883
Paul Wolfe <P.Wolfe@hartwigmedicalfoundation.nl>
2018-06-19 18:59:16 -0400

    Removing hardcoded heap size in start script
    Was used for an experiment. Better to pass java opts to the docker
    container with -e JAVA_OPTS="-Xmx128G"

--- system/bin/pipeline.sh
+++ system/bin/pipeline.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/usr/bin/java -Xmx128G -jar /usr/share/pipeline2/system.jar
+/usr/bin/java -jar /usr/share/pipeline2/system.jar
 status=$?
 if [ $status -ne 0 ]; then
   echo "Failed to start pipeline: $status"
