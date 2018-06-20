bef816fd02c1165e269fe874affa599224f9c6f2
Paul Wolfe <P.Wolfe@hartwigmedicalfoundation.nl>
2018-06-20 14:51:40 -0400

    GC Tuning
    First step in GC tuning, collect metrics. This change enables GC logging
    and appends it to its own file in the log directory.
    
    As per Spark tuning recommendations we switch to the Garbage First
    collector, which is actually the default in Java 9.

--- system/bin/pipeline.sh
+++ system/bin/pipeline.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/usr/bin/java -jar /usr/share/pipeline2/system.jar
+/usr/bin/java -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -Xloggc:logs/gc.log -XX:+UseG1GC -jar /usr/share/pipeline2/system.jar
 status=$?
 if [ $status -ne 0 ]; then
   echo "Failed to start pipeline: $status"
