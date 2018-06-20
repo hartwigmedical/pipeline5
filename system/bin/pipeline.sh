e3ee18b7817dcc2bdd8e9207be3d790d10e27064
Paul Wolfe <P.Wolfe@hartwigmedicalfoundation.nl>
2018-06-20 15:41:01 -0400

    GC Tuning
    While checking GC setting I noticed we were not using the 128G heap I
    had specified. I mistakenly had thought that JAVA_OPTS were used by
    the jvm by default. Added to the script to append to other args.
    
    Also left in a printflags to be absolutely sure, will remove later.

--- system/bin/pipeline.sh
+++ system/bin/pipeline.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-/usr/bin/java -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -Xloggc:logs/gc.log -XX:+UseG1GC -jar /usr/share/pipeline2/system.jar
+/usr/bin/java ${JAVA_OPTS} \
+    -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -Xloggc:logs/gc.log -XX:+UseG1GC \
+     -XX:+PrintFlagsFinal -jar /usr/share/pipeline2/system.jar
 status=$?
 if [ $status -ne 0 ]; then
   echo "Failed to start pipeline: $status"
