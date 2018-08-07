42543d8ff9ada326ea9eb92735ce8618940afc75
Paul Wolfe <P.Wolfe@hartwigmedicalfoundation.nl>
2018-08-07 08:06:33 -0400

    DEV-462 Group by read name before flat map
    Another try, this time to find the overlaps before we do the flat map
    by grouping alignments of the same read name and only using a position
    once.

--- adam/src/main/java/com/hartwig/pipeline/adam/CoverageRDD.java
+++ adam/src/main/java/com/hartwig/pipeline/adam/CoverageRDD.java
@@ -3,11 +3,11 @@ package com.hartwig.pipeline.adam;
 import static htsjdk.samtools.SAMRecord.getReadPositionAtReferencePosition;
 import static htsjdk.samtools.SAMUtils.fastqToPhred;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.rdd.RDD;
 import org.bdgenomics.adam.models.Coverage;
 import org.jetbrains.annotations.NotNull;
@@ -19,22 +19,29 @@ import scala.Tuple2;
 class CoverageRDD {
 
     static JavaRDD<Coverage> toCoverage(final String contig, final RDD<SAMRecordWritable> samRecordRDD) {
-        return samRecordRDD.toJavaRDD().map(SAMRecordWritable::get).flatMapToPair(CoverageRDD::basesWithinRead).reduceByKey((v1, v2) -> v1)
-                .values()
+        return samRecordRDD.toJavaRDD()
+                .map(SAMRecordWritable::get)
+                .mapToPair(record -> Tuple2.apply(record.getReadName(), record))
+                .groupByKey()
+                .flatMap(toOverlappingPositionsWithMinimumBases())
                 .mapToPair(index -> Tuple2.apply(index, 1))
                 .reduceByKey((v1, v2) -> v1 + v2)
                 .map(tuple -> new Coverage(contig, tuple._1, tuple._1 + 1, tuple._2));
     }
 
     @NotNull
-    private static Iterator<Tuple2<Tuple2<Integer, Integer>, Integer>> basesWithinRead(final SAMRecord record) {
-        List<Tuple2<Tuple2<Integer, Integer>, Integer>> basesAndReadNames = new ArrayList<>();
-        for (int index = record.getStart(); index < record.getEnd(); index++) {
-            if (baseQualityAtLeastTen(record, index)) {
-                basesAndReadNames.add(Tuple2.apply(Tuple2.apply(record.getReadName().hashCode(), index), index));
+    private static FlatMapFunction<Tuple2<String, Iterable<SAMRecord>>, Integer> toOverlappingPositionsWithMinimumBases() {
+        return tuple -> {
+            Set<Integer> positions = new HashSet<>();
+            for (SAMRecord record : tuple._2) {
+                for (int index = record.getStart(); index < record.getEnd(); index++) {
+                    if (baseQualityAtLeastTen(record, index)) {
+                        positions.add(index);
+                    }
+                }
             }
-        }
-        return basesAndReadNames.iterator();
+            return positions.iterator();
+        };
     }
 
     private static boolean baseQualityAtLeastTen(final SAMRecord record, final int index) {
