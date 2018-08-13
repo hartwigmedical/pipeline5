196f1e73d115715daf471f2c20cd2d66d7d9c577
Paul Wolfe <P.Wolfe@hartwigmedicalfoundation.nl>
2018-08-12 21:58:27 -0400

    DEV-481 Typo in gcloud bucket name

--- system/spark-kubernetes/spark-2.3.0-bin-hadoop2.7/download-static-data.sh
+++ system/spark-kubernetes/spark-2.3.0-bin-hadoop2.7/download-static-data.sh
@@ -4,6 +4,6 @@ gcloud auth activate-service-account --key-file keys/gcloud-read-credentials.jso
 gcloud config set project hmf-pipeline-development
 
 mkdir /reference_genome
-gsutil rsync gs://reference_genome/ /reference_genome/
+gsutil rsync gs://reference-genome/ /reference_genome/
 mkdir /known_indels
-gsutil rsync gs://known_indels/ /known_indels/
+gsutil rsync gs://known-indels/ /known_indels/
