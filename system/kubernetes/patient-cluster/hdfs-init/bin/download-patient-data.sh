998ec8a2d181a1d2daab3ee0f34a4d6429d5df51
Paul Wolfe <P.Wolfe@hartwigmedicalfoundation.nl>
2018-08-14 13:47:05 -0400

    DEV-483 supporting image version
    Create an HDFS image with a version 1.0 which is functional and refere
    nce it in the namenode.yaml

--- system/kubernetes/patient-cluster/hdfs-init/bin/download-patient-data.sh
+++ system/kubernetes/patient-cluster/hdfs-init/bin/download-patient-data.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-PATIENT=CPCT12345678
+PATIENT=$1
 
 gcloud auth activate-service-account --key-file /root/servicekey/key.json
 gcloud config set project hmf-pipeline-development
 
 mkdir /patients
 gsutil cp -r gs://patients-pipeline5/${PATIENT} /patients
-hadoop fs -mkdir /patients
-hadoop fs -put /patients/* /patients/
