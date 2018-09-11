62ab495944ed5798d82d61059b345c765a0ba74b
Paul Wolfe <P.Wolfe@hartwigmedicalfoundation.nl>
2018-09-11 11:46:19 -0400

    DEV-507 Install bwa 0.7.17
    Extend the install bwa script used for dataproc initialization to
    build bwa from source, and therefore able to install any version.
    
    Currently this script is uploaded by hand into GS, but the idea will be
    to tie this into the build and upload at patient cluster creation.

--- patient-cluster/src/main/resources/install-bwa.sh
+++ patient-cluster/src/main/resources/install-bwa.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-apt-get install bwa
+apt-get update && apt-get install -y build-essential gcc-multilib apt-utils zlib1g-dev wget git
+git clone https://github.com/lh3/bwa.git
+cd bwa
+git checkout 0.7.17
+make
+cp -p bwa /usr/local/bin
