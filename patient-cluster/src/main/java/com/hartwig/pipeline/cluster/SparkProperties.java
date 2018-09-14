0de98d5c91a5792b1d938f86788ff74561a1886a
Paul Wolfe <P.Wolfe@hartwigmedicalfoundation.nl>
2018-09-14 11:43:36 -0400

    DEV-507 Tuning for GIAB
    Turn off dynamic allocation as we'd prefer to precisely tune the amount
    of executors, memory and cpus.

--- patient-cluster/src/main/java/com/hartwig/pipeline/cluster/SparkProperties.java
+++ patient-cluster/src/main/java/com/hartwig/pipeline/cluster/SparkProperties.java
@@ -9,7 +9,10 @@ class SparkProperties {
     static Map<String, String> asMap() {
         return ImmutableMap.<String, String>builder().put("spark.yarn.executor.memoryOverhead", "10G")
                 .put("spark.driver.memory", "64G")
-                .put("spark.executor.memory", "64G")
+                .put("spark.executor.memory", "40G")
+                .put("spark.dynamicAllocation.enabled", "false")
+                .put("spark.executor.instances", "8")
+                .put("spark.executor.cores", "15")
                 .build();
     }
 }
