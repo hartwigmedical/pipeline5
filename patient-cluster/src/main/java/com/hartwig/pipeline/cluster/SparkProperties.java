545d938f39beaba44fed11af4e947e0ce24fe3f0
Paul Wolfe <P.Wolfe@hartwigmedicalfoundation.nl>
2018-09-17 13:40:35 -0400

    DEV-507 Reduce memory per executor
    Current settings exceed allowable for a 6 node cluster of 32 cpu boxes.

--- patient-cluster/src/main/java/com/hartwig/pipeline/cluster/SparkProperties.java
+++ patient-cluster/src/main/java/com/hartwig/pipeline/cluster/SparkProperties.java
@@ -7,8 +7,9 @@ import com.google.common.collect.ImmutableMap;
 class SparkProperties {
 
     static Map<String, String> asMap() {
-        return ImmutableMap.<String, String>builder().put("spark.yarn.executor.memoryOverhead", "40G")
-                .put("spark.driver.memory", "64G").put("spark.executor.memory", "60G")
+        return ImmutableMap.<String, String>builder().put("spark.yarn.executor.memoryOverhead", "35G")
+                .put("spark.driver.memory", "64G")
+                .put("spark.executor.memory", "50G")
                 .put("spark.dynamicAllocation.enabled", "false").put("spark.executor.instances", "6").put("spark.executor.cores", "25")
                 .build();
     }
