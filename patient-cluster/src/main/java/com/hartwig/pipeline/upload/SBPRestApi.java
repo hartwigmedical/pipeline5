303d32027944fae4bdef55713e94bc0320fc72f5
Paul Wolfe <P.Wolfe@hartwigmedicalfoundation.nl>
2018-09-20 16:02:30 -0400

    DEV-523 Fix issues with PATCH and http client
    Java does not support PATCH as an http method out of the box, and fails
    hard when you try. Jersey supports a workaround which we take advantage
    of here.
    
    This requires us to be a little more explicit in our dependencies,
    ensuring we use a modern jersey-client and jax-rs api.

--- patient-cluster/src/main/java/com/hartwig/pipeline/upload/SBPRestApi.java
+++ patient-cluster/src/main/java/com/hartwig/pipeline/upload/SBPRestApi.java
@@ -26,7 +26,7 @@ public class SBPRestApi {
     }
 
     String getFastQ(int sampleId) {
-        LOGGER.info("Connecting to SBP at [{}] for sample id [{}]", target.getUri(), sampleId);
+        LOGGER.info("Connecting to SBP API at [{}] for sample id [{}]", target.getUri(), sampleId);
         Response response = target.path("hmf").path("v1").path("fastq").queryParam("sample_id", sampleId).request().buildGet().invoke();
         if (response.getStatus() == Response.Status.OK.getStatusCode()) {
             return response.readEntity(String.class);
