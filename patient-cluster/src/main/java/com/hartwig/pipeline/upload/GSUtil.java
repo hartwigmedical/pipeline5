e26c2ef4586a7199290c1c42142c724b996b0c75
Paul Wolfe <P.Wolfe@hartwigmedicalfoundation.nl>
2018-10-09 08:13:47 -0400

    Removing output redirects
    Look confusing and messy. Better to use our own logging statements
    to keep a coherent format.

--- patient-cluster/src/main/java/com/hartwig/pipeline/upload/GSUtil.java
+++ patient-cluster/src/main/java/com/hartwig/pipeline/upload/GSUtil.java
@@ -9,18 +9,14 @@ public class GSUtil {
     public static void auth(String gsdkPath, String keyFile) throws IOException, InterruptedException {
         ProcessBuilder processBuilder =
                 new ProcessBuilder(gsdkPath + "/gcloud", "auth", "activate-service-account", String.format("--key-file=%s", keyFile));
-        processBuilder.redirectOutput(ProcessBuilder.Redirect.INHERIT);
-        processBuilder.redirectError(ProcessBuilder.Redirect.INHERIT);
         int exitCode = processBuilder.start().waitFor();
         if (exitCode != 0) {
             throw new RuntimeException(String.format("GCloud auth returned a non-zero exit code of [%s]. Unable to continue", exitCode));
         }
     }
 
-    public static void cp(String gsdkPath, String sourceUrl, String targetUrl) throws IOException, InterruptedException {
+    static void cp(String gsdkPath, String sourceUrl, String targetUrl) throws IOException, InterruptedException {
         ProcessBuilder processBuilder = new ProcessBuilder(gsdkPath + "/gsutil", "-m", "cp", sourceUrl, targetUrl);
-        processBuilder.redirectError(ProcessBuilder.Redirect.INHERIT);
-        processBuilder.redirectOutput(ProcessBuilder.Redirect.INHERIT);
         int exitValue = processBuilder.start().waitFor();
         if (exitValue != 0) {
             throw new RuntimeException(format("gsutil exited with a non-zero error code [%s]", exitValue));
