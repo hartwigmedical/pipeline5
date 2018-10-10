e58422d19329d69206886c642c83fc39896f4dd0
Paul Wolfe <P.Wolfe@hartwigmedicalfoundation.nl>
2018-10-10 17:19:08 -0400

    Use correct version of BWA on travis
    Permission issue calling the node-init script

--- travis-init.sh
+++ travis-init.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 # Piggy-back on the dataproc node init to keep bwa in synch
+chmod 777 ./patient-cluster/src/main/resources/node-init.sh
 ./patient-cluster/src/main/resources/node-init.sh
