405f8fb1fd84278cb353abc4a16f455c106c7652
Paul Wolfe <P.Wolfe@hartwigmedicalfoundation.nl>
2018-10-21 11:55:54 -0400

    DEV-566 Back to standard worker

--- cluster/src/main/java/com/hartwig/pipeline/performance/MachineType.java
+++ cluster/src/main/java/com/hartwig/pipeline/performance/MachineType.java
@@ -8,6 +8,7 @@ public interface MachineType {
 
     String GOOGLE_STANDARD_16 = "n1-standard-16";
     String GOOGLE_STANDARD_32 = "n1-standard-32";
+    String GOOGLE_STANDARD_64 = "n1-standard-64";
     String GOOGLE_HIGHMEM_32 = "n1-highmem-32";
     int DISK_GB = 1000;
 
@@ -18,6 +19,10 @@ public interface MachineType {
     int cpus();
 
     int diskGB();
+
+    static MachineType beefy() {
+        return ImmutableMachineType.builder().uri(GOOGLE_STANDARD_64).memoryGB(240).cpus(64).diskGB(DISK_GB).build();
+    }
 
     static MachineType defaultWorker() {
         return highMemoryWorker();
