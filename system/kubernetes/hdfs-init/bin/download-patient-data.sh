4f0234d3ce0f12a324698fb7b2a17a410a41dddb
Paul Wolfe <P.Wolfe@hartwigmedicalfoundation.nl>
2018-10-22 14:12:04 -0400

    Remove all unused code from system and give sbp code own module
    Tidy up unused code and take the first step to a python module/
    CI/CD setup for the polling container and other SBP specific code.

--- system/kubernetes/hdfs-init/bin/download-patient-data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-PATIENT=$1
-
-gcloud auth activate-service-account --key-file /root/servicekey/key.json
-gcloud config set project hmf-pipeline-development
-
-gsutil -m cp -r gs://patients-pipeline5/${PATIENT} /patients
