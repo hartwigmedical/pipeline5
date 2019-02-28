86e51ea993f5fa4c7b652cde3d2769cbc2668237
Paul Wolfe <P.Wolfe@hartwigmedicalfoundation.nl>
2019-02-28 11:30:02 -0500

    Help Korneelius with his test
    Create a resources path in the bam module with the source BAM and
    extract a constant.

--- bam/src/test/java/com/hartwig/pipeline/after/BamMetricsPipelineTest.java
+++ bam/src/test/java/com/hartwig/pipeline/after/BamMetricsPipelineTest.java
@@ -15,23 +15,20 @@ import com.hartwig.support.test.Resources;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
-@Ignore("work in progress")
 public class BamMetricsPipelineTest {
 
     private static final String SAMPLE_NAME = "CPCT12345678R";
     private static final String TEST_DIR = "metric_test";
-    private static final String SOURCE_DIR = "metrics";
+    private static final String SOURCE_DIR = "metrics/";
     private static final String OUTPUT_FILE = System.getProperty("user.dir") + "/CPCT1234567R";
     private Metric metricTimeSpent;
 
     @Before
     public void setUp() throws Exception {
         final Path testDir = Paths.get(Resources.targetResource(TEST_DIR));
-        final BamMetricsPipeline victim = BamMetricsPipeline.create(Hadoop.localFilesystem(),
-                Resources.testResource("metrics"),
+        final BamMetricsPipeline victim = BamMetricsPipeline.create(Hadoop.localFilesystem(), Resources.testResource(SOURCE_DIR),
                 Resources.testResource("reference_genome"),
                 metric -> metricTimeSpent = metric);
         FileUtils.deleteDirectory(testDir.toFile());
@@ -53,7 +50,7 @@ public class BamMetricsPipelineTest {
     private void moveTestFilesToTarget(final Path resultDir) throws IOException {
         Files.createDirectory(resultDir);
         Files.createDirectory(Paths.get(resultDir.toString(), SOURCE_DIR));
-        Files.copy(Paths.get(Resources.testResource("metric/" + SAMPLE_NAME + "sorted.bam")),
-                Paths.get(resultDir.toString(), SOURCE_DIR, SAMPLE_NAME + "sorted.bam"));
+        Files.copy(Paths.get(Resources.testResource(SOURCE_DIR + SAMPLE_NAME + ".sorted.bam")),
+                Paths.get(resultDir.toString(), SOURCE_DIR, SAMPLE_NAME + ".sorted.bam"));
     }
 }
