749689a3753ba98e868e28ee67ad2d54f1838a1b
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2019-02-28 14:30:13 +0100

    DEV-666: Add picard to every node (dont run it yet)

--- cluster/src/main/resources/node-init.sh
+++ cluster/src/main/resources/node-init.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
 
-install_bwa(){
+setup_environment() {
     apt-get update && apt-get install -y build-essential gcc-multilib apt-utils zlib1g-dev wget git
+}
+
+install_bwa() {
     git clone https://github.com/lh3/bwa.git
     cd bwa
     git checkout 0.7.17
     make
     cp -p bwa /usr/local/bin
+}
+
+install_picard() {
+    git clone https://github.com/broadinstitute/picard.git
+    cd picard
+    git checkout 2.18.27
+    ./gradlew shadowJar
+    cp build/libs/picard.jar /usr/local/bin/picard.jar
 }
 
 install_sambamba() {
@@ -15,5 +26,7 @@ install_sambamba() {
     chmod 777 sambamba-0.6.8-linux-static
     mv sambamba-0.6.8-linux-static /usr/local/bin/sambamba
 }
+
+setup_environment
 install_bwa
 install_sambamba
