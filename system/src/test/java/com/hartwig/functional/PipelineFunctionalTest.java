e3c7f2b6377c6acb49b2cb63fe8d6bcbd979254f
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2019-03-20 10:42:54 +0100

    Create context similarly to other test classes and ignore test

--- system/src/test/java/com/hartwig/functional/PipelineFunctionalTest.java
+++ system/src/test/java/com/hartwig/functional/PipelineFunctionalTest.java
@@ -19,6 +19,7 @@ import org.apache.spark.api.java.JavaSparkContext;
 import org.bdgenomics.adam.rdd.ADAMContext;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,28 +30,31 @@ public class PipelineFunctionalTest {
 
     private static final Sample REFERENCE_SAMPLE =
             Sample.builder(HUNDREDK_READS_HISEQ.patient().directory(), HUNDREDK_READS_HISEQ_PATIENT_NAME + "R").build();
-    private static JavaSparkContext context;
+    private static final JavaSparkContext SPARK_CONTEXT = SparkContexts.create("function-test", HUNDREDK_READS_HISEQ);
 
     private static final String RESULT_DIR = System.getProperty("user.dir") + "/results/";
 
     @BeforeClass
     public static void beforeClass() throws Exception {
-        context = SparkContexts.create("function-test", HUNDREDK_READS_HISEQ);
         LOGGER.info("Clearing out bwa shared memory before running test");
         Processes.run(new ProcessBuilder("bwa", "shm", "-d"));
     }
 
     @AfterClass
     public static void afterClass() {
-        context.stop();
+        SPARK_CONTEXT.stop();
     }
 
     @Test
+    @Ignore
     public void adamBamCreationMatchesCurrentPipelineOutput() throws Exception {
         FileSystem fileSystem = Hadoop.localFilesystem();
-        Pipelines.bamCreationConsolidated(new ADAMContext(context.sc()),
+        Pipelines.bamCreationConsolidated(new ADAMContext(SPARK_CONTEXT.sc()),
                 fileSystem,
-                Monitor.noop(), RESULT_DIR, HUNDREDK_READS_HISEQ.referenceGenome().path(), Collections.emptyList(),
+                Monitor.noop(),
+                RESULT_DIR,
+                HUNDREDK_READS_HISEQ.referenceGenome().path(),
+                Collections.emptyList(),
                 HUNDREDK_READS_HISEQ.knownSnp().paths(),
                 1,
                 false,
