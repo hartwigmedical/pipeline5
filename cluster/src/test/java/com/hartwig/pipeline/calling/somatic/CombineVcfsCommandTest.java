3775f363a1c8a286e6f756048d1da935485ef1a3
Paul Wolfe <P.Wolfe@hartwigmedicalfoundation.nl>
2019-04-19 08:19:54 -0400

    DEV-708 Fix failing test
    After adding version to the command string.

--- cluster/src/test/java/com/hartwig/pipeline/calling/somatic/CombineVcfsCommandTest.java
+++ cluster/src/test/java/com/hartwig/pipeline/calling/somatic/CombineVcfsCommandTest.java
@@ -10,7 +10,7 @@ public class CombineVcfsCommandTest {
     public void createsBashForCombineVcfsGatkAnalysis() {
         CombineVcfsCommand victim =
                 new CombineVcfsCommand("reference/genome/path.fasta", "/data/output/snvs.vcf", "/data/output/indels.vcf", "/data/output");
-        assertThat(victim.asBash()).isEqualTo("java -Xmx20G -jar /data/tools/GenomeAnalysisTK.jar -T CombineVariants "
+        assertThat(victim.asBash()).isEqualTo("java -Xmx20G -jar /data/tools/gatk/3.8.0/GenomeAnalysisTK.jar -T CombineVariants "
                 + "-R reference/genome/path.fasta --genotypemergeoption unsorted -V:snvs /data/output/snvs.vcf -V:indels "
                 + "/data/output/indels.vcf -o /data/output/combined.vcf");
     }
