96ee02d4f20f5edcc6364127efbab9f8d8a2e75d
Paul Wolfe <P.Wolfe@hartwigmedicalfoundation.nl>
2019-04-22 17:48:52 -0400

    Fix failing test

--- cluster/src/test/java/com/hartwig/pipeline/calling/somatic/CombineVcfsCommandTest.java
+++ cluster/src/test/java/com/hartwig/pipeline/calling/somatic/CombineVcfsCommandTest.java
@@ -8,8 +8,10 @@ public class CombineVcfsCommandTest {
 
     @Test
     public void createsBashForCombineVcfsGatkAnalysis() {
-        CombineVcfsCommand victim =
-                new CombineVcfsCommand("reference/genome/path.fasta", "/data/output/snvs.vcf", "/data/output/indels.vcf", "/data/output");
+        CombineVcfsCommand victim = new CombineVcfsCommand("reference/genome/path.fasta",
+                "/data/output/snvs.vcf",
+                "/data/output/indels.vcf",
+                "/data/output/combined.vcf");
         assertThat(victim.asBash()).isEqualTo("java -Xmx20G -jar /data/tools/gatk/3.8.0/GenomeAnalysisTK.jar -T CombineVariants "
                 + "-R reference/genome/path.fasta --genotypemergeoption unsorted -V:snvs /data/output/snvs.vcf -V:indels "
                 + "/data/output/indels.vcf -o /data/output/combined.vcf");
