e43451dc35bc82396735845c1cd93c88cecdcf00
Ned Leitch <N.Leitch@hartwigmedicalfoundation.nl>
2019-05-29 06:23:30 -0400

    DEV-691 Stop requiring argument for disk image

--- cluster/images/create_custom_image.sh
+++ cluster/images/create_custom_image.sh
@@ -9,9 +9,9 @@ GCL="gcloud compute --project=${PROJECT}"
 which gcloud 2>&1 >/dev/null
 [[ $? -ne 0 ]] && echo "gcloud is missing" >&2 && exit 1
 
-[[ $# -ne 1 ]] && echo "Provide the instance type to create" >&2 && exit 1
-type="$1"
-[[ ! -f ${type}.cmds ]] && echo "Unknown instance type '${type}'!" >&2 && exit 1
+type="standard"
+cmds="${type}.cmds"
+[[ ! -f ${cmds} ]] && echo "Unknown instance type '${type}'!" >&2 && exit 1
 sourceInstance="diskimager-${type}"
 
 # From here on in we want to fail immediately on any surprises
@@ -23,7 +23,7 @@ echo "#!$(which sh) -e"
 echo "$GCL instances create ${sourceInstance} --description=\"Instance for ${type} disk image creation\" --zone=${ZONE}"
 
 # Ignore lines consisting only defaultDirectory spaces, or those beginning with '#'
-egrep -v '^#|^ *$' ${type}.cmds | while read cmd
+egrep -v '^#|^ *$' ${cmds} | while read cmd
 do
   echo "$GCL ssh ${sourceInstance} --zone=${ZONE} --command=\"$cmd\""
 done
