8626cd9be3beadbc7f3dc4ed681a9c1b10dedc75
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2019-06-13 13:58:05 +0200

    Remove static final from constants in an interface (these classifiers don't do anything)

--- cluster/src/main/java/com/hartwig/pipeline/execution/vm/VmDirectories.java
+++ cluster/src/main/java/com/hartwig/pipeline/execution/vm/VmDirectories.java
@@ -1,10 +1,10 @@
 package com.hartwig.pipeline.execution.vm;
 
 public interface VmDirectories {
-    static final String INPUT = "/data/input";
-    static final String OUTPUT = "/data/output";
-    static final String RESOURCES = "/data/resources";
-    static final String TOOLS = "/data/tools";
+    String INPUT = "/data/input";
+    String OUTPUT = "/data/output";
+    String RESOURCES = "/data/resources";
+    String TOOLS = "/data/tools";
 
     static String outputFile(String path) {
         return String.format("%s/%s", OUTPUT, path);
