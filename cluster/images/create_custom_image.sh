2c6b2a86d25e7bca793f763caa95cf0d3764568e
Ned Leitch <N.Leitch@hartwigmedicalfoundation.nl>
2019-07-03 14:44:50 -0400

    DEV-807 Wrap image generation script to eliminate pipe to sh

--- cluster/images/create_custom_image.sh
+++ cluster/images/create_custom_image.sh
@@ -1,33 +1,12 @@
 #!/usr/bin/env bash
-#
-# Generate a creation script for a VM and image
 
-ZONE="europe-west4-a"
-PROJECT="hmf-pipeline-development"
-GCL="gcloud compute --project=${PROJECT}"
+set -e
 
-which gcloud 2>&1 >/dev/null
-[[ $? -ne 0 ]] && echo "gcloud is missing" >&2 && exit 1
+script="$(mktemp)"
+touch "${script}"
+"$(dirname "$0")/generate_imaging_script.sh" > ${script}
+echo "Executing generated script ${script}"
+chmod +x ${script}
+${script}
+rm ${script}
 
-type="standard"
-cmds="${type}.cmds"
-[[ ! -f ${cmds} ]] && echo "Unknown instance type '${type}'!" >&2 && exit 1
-sourceInstance="diskimager-${type}"
-
-# From here on in we want to fail immediately on any surprises
-echo "#!$(which sh) -e"
-
-# Defaults to Debian 9; we assume that for now
-# Also this will fail if there are underscores in the the name, and will succeed if there are hyphens, which doesn't really agree
-# with the regex given in the help
-echo "$GCL instances create ${sourceInstance} --description=\"Instance for ${type} disk image creation\" --zone=${ZONE}"
-
-# Ignore lines consisting only of spaces, or those beginning with '#'
-egrep -v '^#|^ *$' ${cmds} | while read cmd
-do
-  echo "$GCL ssh ${sourceInstance} --zone=${ZONE} --command=\"$cmd\""
-done
-
-echo "$GCL instances stop ${sourceInstance} --zone=${ZONE}"
-echo "$GCL images create ${sourceInstance}-$(date +%Y%m%d%H%M) --family=${sourceInstance} --source-disk=${sourceInstance} --source-disk-zone=${ZONE}"
-echo "$GCL instances -q delete ${sourceInstance} --zone=${ZONE}"
