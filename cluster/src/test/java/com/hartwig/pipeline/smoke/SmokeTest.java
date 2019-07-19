ca0d8265818b167866b812fa4fff75022ada9654
Paul Wolfe <P.Wolfe@hartwigmedicalfoundation.nl>
2019-07-19 16:53:19 -0400

    DEV-841 Add smoke test for docker profile
    Will run a simple smoke test consisting of starting the full pipeline
    and asserting that the pipeline succeeeds.
    
    Credentials are passed in via an encrypted private key file. The file
    is encrypted with travis who also holds the decryption key.
    
    A junit Category is used to separate this test from the rest. Currently
    disabled to test running in travis as part of the PR build.

--- cluster/src/test/java/com/hartwig/pipeline/smoke/SmokeTest.java
+++ cluster/src/test/java/com/hartwig/pipeline/smoke/SmokeTest.java
@@ -6,7 +6,6 @@ import com.hartwig.pipeline.Arguments;
 import com.hartwig.pipeline.PipelineMain;
 import com.hartwig.pipeline.PipelineState;
 import com.hartwig.pipeline.execution.PipelineStatus;
-import com.hartwig.pipeline.tools.Versions;
 
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -17,24 +16,20 @@ public class SmokeTest {
     @Test
     public void runFullPipelineAndAssertNoErrors() {
         PipelineMain victim = new PipelineMain();
+        String version = System.getProperty("version");
         PipelineState state = victim.start(Arguments.defaultsBuilder(Arguments.DefaultsProfile.DEVELOPMENT.toString())
                 .privateKeyPath("google-key.json")
                 .sampleDirectory(System.getProperty("user.dir") + "/../samples")
-                .version(System.getProperty("version"))
+                .version(version)
                 .jarDirectory(System.getProperty("user.dir") + "/../system/target")
                 .cloudSdkPath("/usr/bin")
                 .nodeInitializationScript(System.getProperty("user.dir") + "/src/main/resources/node-init.sh")
                 .setId("CPCT12345678")
                 .mode(Arguments.Mode.FULL)
-                .runId("smoke-test-" + version())
+                .runId("smoke-" + version)
                 .runGermlineCaller(false)
                 .cleanup(false)
                 .build());
         assertThat(state.status()).isEqualTo(PipelineStatus.SUCCESS);
-    }
-
-    private String version() {
-        String version = Versions.class.getPackage().getImplementationVersion();
-        return version != null ? version : System.getProperty("user.name");
     }
 }
