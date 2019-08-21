bac5779b79de44edfc9a92b99fc5d8e0804c5a32
Paul Wolfe <P.Wolfe@hartwigmedicalfoundation.nl>
2019-08-21 09:49:05 -0400

    DEV-892 Remove all files except status files before bam creation
    Removes all files not suffixed with _SUCCESS or _FAILURE before starting
    the bam creation.

--- bam/src/test/java/com/hartwig/bam/adam/HDFSBamStoreTest.java
+++ bam/src/test/java/com/hartwig/bam/adam/HDFSBamStoreTest.java
@@ -11,11 +11,13 @@ import com.hartwig.support.hadoop.Hadoop;
 import com.hartwig.testsupport.TestConfigurations;
 import com.hartwig.testsupport.TestRDDs;
 
+import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.bdgenomics.adam.rdd.read.AlignmentRecordDataset;
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.Test;
 
 public class HDFSBamStoreTest {
@@ -23,6 +25,17 @@ public class HDFSBamStoreTest {
     private static final JavaSparkContext SPARK_CONTEXT =
             SparkContexts.create("hdfs-bam-store-test", TestConfigurations.HUNDREDK_READS_HISEQ);
     private static final Sample SAMPLE = Sample.builder("", "test").build();
+    private static final String RESULTS_PATH = System.getProperty("user.dir") + "/results/";
+    private FinalDataLocation dataLocation;
+    private FileSystem fileSystem;
+    private OutputStore<AlignmentRecordDataset> victim;
+
+    @Before
+    public void setUp() throws Exception {
+        fileSystem = Hadoop.localFilesystem();
+        dataLocation = new FinalDataLocation(fileSystem, RESULTS_PATH);
+        victim = new HDFSBamStore(dataLocation, fileSystem, true);
+    }
 
     @AfterClass
     public static void afterClass() {
@@ -31,11 +44,20 @@ public class HDFSBamStoreTest {
 
     @Test
     public void savesBamAndChecksIfItExists() throws Exception {
-        FileSystem fileSystem = Hadoop.localFilesystem();
-        FinalDataLocation dataLocation = new FinalDataLocation(fileSystem, System.getProperty("user.dir") + "/results/");
-        OutputStore<AlignmentRecordDataset> victim = new HDFSBamStore(dataLocation, fileSystem, true);
         victim.store(InputOutput.of(SAMPLE, TestRDDs.alignmentRecordDataset("qc/CPCT12345678R.bam", SPARK_CONTEXT)));
         assertThat(victim.exists(SAMPLE)).isTrue();
         fileSystem.delete(new Path(dataLocation.uri(SAMPLE, "")), true);
+    }
+
+    @Test
+    public void clearsEverythingInStoreExceptStatusFiles() throws Exception{
+        fileSystem.create(new Path(RESULTS_PATH + "/file1.txt"));
+        fileSystem.create(new Path(RESULTS_PATH + "/Gunzip_SUCCESS"));
+        fileSystem.create(new Path(RESULTS_PATH + "/BamCreation_FAILURE"));
+        victim.clear();
+
+        FileStatus[] fileStatuses = fileSystem.listStatus(new Path(RESULTS_PATH));
+        assertThat(fileStatuses).hasSize(2);
+        fileSystem.delete(new Path(RESULTS_PATH), true);
     }
 }
