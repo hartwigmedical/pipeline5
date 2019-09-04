c530fd47173969e0c3517b30187bb2e518eddbf7
Paul Wolfe <P.Wolfe@hartwigmedicalfoundation.nl>
2019-09-04 10:00:21 +0200

    Trap exceptions in the full pipeline futures so we can see any errors.

--- cluster/src/main/java/com/hartwig/pipeline/FullPipeline.java
+++ cluster/src/main/java/com/hartwig/pipeline/FullPipeline.java
@@ -6,7 +6,12 @@ import java.util.concurrent.ExecutorService;
 import com.hartwig.pipeline.metadata.CompletionHandler;
 import com.hartwig.pipeline.metadata.LocalSampleMetadataApi;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class FullPipeline {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(FullPipeline.class);
 
     private final SingleSamplePipeline referencePipeline;
     private final SingleSamplePipeline tumorPipeline;
@@ -34,14 +39,23 @@ public class FullPipeline {
         CountDownAndTrapStatus trapTumor = new CountDownAndTrapStatus(bothSingleSamplesComplete);
         referenceApi.register(trapReference);
         tumorApi.register(trapTumor);
-        executorService.submit(referencePipeline::run);
-        executorService.submit(tumorPipeline::run);
+        executorService.submit(() -> runPipeline(referencePipeline, bothSingleSamplesComplete));
+        executorService.submit(() -> runPipeline(tumorPipeline, bothSingleSamplesComplete));
         waitForSingleSamples(bothSingleSamplesComplete);
         PipelineState singleSampleState = combine(trapReference, trapTumor);
         if (singleSampleState.shouldProceed()) {
             return singleSampleState.combineWith(somaticPipeline.run());
         } else {
             return singleSampleState;
+        }
+    }
+
+    private void runPipeline(SingleSamplePipeline pipeline, CountDownLatch latch) {
+        try {
+            pipeline.run();
+        }catch (Exception e){
+            LOGGER.error("Could not run single sample pipeline. ", e);
+            latch.countDown();
         }
     }
 
