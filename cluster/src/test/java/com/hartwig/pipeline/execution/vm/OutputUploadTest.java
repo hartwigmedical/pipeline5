0a976dbdc6376ecfac7a6eec33ca11939a1df00e
Ned Leitch <N.Leitch@hartwigmedicalfoundation.nl>
2019-11-08 10:27:16 -0500

    Merge DEV-999 into 5.5-hotfix

--- cluster/src/test/java/com/hartwig/pipeline/execution/vm/OutputUploadTest.java
+++ cluster/src/test/java/com/hartwig/pipeline/execution/vm/OutputUploadTest.java
@@ -1,7 +1,5 @@
 package com.hartwig.pipeline.execution.vm;
 
-import static java.lang.String.format;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.hartwig.pipeline.calling.structural.gridss.CommonEntities;
@@ -13,8 +11,7 @@ public class OutputUploadTest implements CommonEntities {
 
     @Test
     public void createsBaseToCopyAllFilesAndDirsInOutputFolderToOutputBucket() {
-        OutputUpload victim = new OutputUpload(GoogleStorageLocation.of("bucket", "results/"));
-        assertThat(victim.asBash()).isEqualTo(format("(cp %s %s && gsutil -qm cp -r %s/ gs://bucket/results/)",
-                LOG_FILE, OUT_DIR, OUT_DIR));
+        OutputUpload victim = new OutputUpload(GoogleStorageLocation.of("bucket", "results"));
+        assertThat(victim.asBash()).isEqualTo("(cp /var/log/run.log /data/output && gsutil -qm rsync -dr /data/output gs://bucket/results)");
     }
 }
