67992740a5650dc54ebc20bfafd892b8c560d950
Paul Wolfe <P.Wolfe@hartwigmedicalfoundation.nl>
2019-11-14 07:12:55 -0500

    Improving GCP transfer logging (add start and finish)

--- cluster/src/main/java/com/hartwig/pipeline/transfer/google/GoogleArchiver.java
+++ cluster/src/main/java/com/hartwig/pipeline/transfer/google/GoogleArchiver.java
@@ -10,23 +10,25 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class GoogleArchiver {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(GoogleArchiver.class);
     private final Arguments arguments;
-    private static final Logger LOGGER = LoggerFactory.getLogger(GoogleArchiver.class);
 
     public GoogleArchiver(Arguments arguments) {
         this.arguments = arguments;
     }
 
     public void transfer(SomaticRunMetadata metadata) {
+        LOGGER.info("Starting transfer from [{}] to GCP bucket [{}]", arguments.patientReportBucket(), arguments.archiveBucket());
         try {
             GSUtil.configure(false, 1);
             GSUtil.auth(arguments.cloudSdkPath(), arguments.archivePrivateKeyPath());
             String source = format("gs://%s/%s", arguments.patientReportBucket(), metadata.runName());
             String destination = format("gs://%s/%s", arguments.archiveBucket(), metadata.runName());
-            LOGGER.info("Copying run output from [{}] to [{}]", source, destination);
             GSUtil.cp(arguments.cloudSdkPath(), source, destination, arguments.archiveProject(), true);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+        LOGGER.info("Transfer complete.");
     }
 }
