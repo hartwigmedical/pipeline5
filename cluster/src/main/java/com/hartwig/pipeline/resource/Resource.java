14439445c3d1de883e303a7f20fc748346bd3c14
Paul Wolfe <P.Wolfe@hartwigmedicalfoundation.nl>
2019-11-25 11:19:49 -0500

    DEV-1056 Fix path to dbsnps

--- cluster/src/main/java/com/hartwig/pipeline/resource/Resource.java
+++ cluster/src/main/java/com/hartwig/pipeline/resource/Resource.java
@@ -1,7 +1,7 @@
 package com.hartwig.pipeline.resource;
 
 import static com.hartwig.pipeline.resource.ResourceNames.COSMIC;
-import static com.hartwig.pipeline.resource.ResourceNames.DBNSFP;
+import static com.hartwig.pipeline.resource.ResourceNames.DBSNPS;
 import static com.hartwig.pipeline.resource.ResourceNames.GC_PROFILE;
 import static com.hartwig.pipeline.resource.ResourceNames.REFERENCE_GENOME;
 import static com.hartwig.pipeline.resource.ResourceNames.SNPEFF;
@@ -18,6 +18,6 @@ public interface Resource {
     String GC_PROFILE_CNP = Resource.of(GC_PROFILE, "GC_profile.1000bp.cnp");
     String SNPEFF_CONFIG = Resource.of(SNPEFF, "snpEff.config");
     String SNPEFF_DB = Resource.of(SNPEFF, "snpEff_v4_3_GRCh37.75.zip");
-    String DBSNPS_VCF = Resource.of(DBNSFP, "dbNSFP2.9.txt.gz");
+    String DBSNPS_VCF = Resource.of(DBSNPS, "dbsnp_137.b37.vcf");
     String COSMIC_VCF_GZ = Resource.of(COSMIC, "CosmicCodingMuts_v85_collapsed.vcf.gz");
 }
