84a443bbc4d9fa901c224f65bc41d706483bbfc1
Paul Wolfe <P.Wolfe@hartwigmedicalfoundation.nl>
2020-01-15 16:35:02 -0500

    bcl2fastq scheduler
    Add sbp url to args

--- sbp/flowcell-check/flowcell.py
+++ sbp/flowcell-check/flowcell.py
@@ -33,7 +33,7 @@ def start_kubernetes_job(args):
     with file('/var/run/secrets/kubernetes.io/serviceaccount/namespace') as f:
         namespace = f.read()
 
-    job_args = ['-flowcell', str(args['flowcell'])]
+    job_args = ['-flowcell', str(args['flowcell']), '-sbp_api_url', os.getenv('HMF_APIHOST')]
 
     for i in range(1, len(sys.argv)):
         job_args.append(sys.argv[i])
