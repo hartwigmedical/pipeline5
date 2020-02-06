dddc336e9ab86a2b4a0a5010b710c2608b1800fe
Jonathan Baber <jonbaber@gmail.com>
2020-02-07 07:20:01 +1000

    DEV-1133: Add new bcf tools builder

---
+++ cluster/src/main/java/com/hartwig/pipeline/calling/command/BcfToolsCommandBuilder.java
@@ -0,0 +1,81 @@
+package com.hartwig.pipeline.calling.command;
+
+import java.util.List;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+import com.hartwig.pipeline.execution.vm.BashCommand;
+import com.hartwig.pipeline.execution.vm.unix.PipeCommands;
+
+public class BcfToolsCommandBuilder  {
+
+    private final String inputVcf;
+    private final String outputVcf;
+    private final List<BashCommand> pipedCommands = Lists.newArrayList();
+    private String currentArguments = null;
+
+    public BcfToolsCommandBuilder(final String inputVcf, final String outputVcf) {
+        this.inputVcf = inputVcf;
+        this.outputVcf = outputVcf;
+    }
+
+    public BcfToolsCommandBuilder includeHardPass() {
+        return includeHardFilter("'FILTER=\"PASS\"'");
+    }
+
+    public BcfToolsCommandBuilder includeHardFilter(String expression) {
+        addArguments("filter", "-i", expression);
+        return this;
+    }
+
+    public BcfToolsCommandBuilder excludeSoftFilter(String expression, String softFilter) {
+        addArguments("filter", "-e", expression, "-s", softFilter, "-m+");
+        return this;
+    }
+
+    public BcfToolsCommandBuilder removeAnnotation(String annotation) {
+        addArguments("annotate", "-x", annotation);
+        return this;
+    }
+
+    public BcfToolsCommandBuilder addAnnotation(String file, String annotation) {
+        addArguments("annotate", "-a", file, "-c", annotation);
+        return this;
+    }
+
+    public BcfToolsCommandBuilder addAnnotation(String file, String annotation, String header) {
+        addArguments("annotate", "-a", file, "-h", header, "-c", annotation);
+        return this;
+    }
+
+    public BcfToolsCommandBuilder selectSample(String tumorSampleName) {
+        addArguments("view", "-s", tumorSampleName);
+        return this;
+    }
+
+    private void addArguments(String... argumentArray) {
+        final String arguments = String.join(" ", argumentArray);
+        if (currentArguments == null) {
+            currentArguments = String.join(" ", Lists.newArrayList(arguments, inputVcf));
+        } else {
+            pipedCommands.add(new BcfToolsCommand(currentArguments, "-O", "u"));
+            currentArguments = arguments;
+        }
+    }
+
+    public BashCommand build() {
+        if (currentArguments == null) {
+            throw new IllegalStateException("No bcftools command added.");
+        }
+
+        final List<BashCommand> finalCommands = Lists.newArrayList(pipedCommands);
+        final BashCommand finalCommand = new BcfToolsCommand(currentArguments, "-O", "z", "-o", outputVcf);
+        finalCommands.add(finalCommand);
+
+        return new PipeCommands(finalCommands.toArray(new BashCommand[finalCommands.size()]));
+    }
+
+    public List<BashCommand> buildWithIndex() {
+        return ImmutableList.of(build(), new TabixCommand(outputVcf));
+    }
+}
---
+++ cluster/src/test/java/com/hartwig/pipeline/calling/command/BcfToolsCommandBuilderTest.java
@@ -0,0 +1,89 @@
+package com.hartwig.pipeline.calling.command;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import com.hartwig.pipeline.execution.vm.BashCommand;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BcfToolsCommandBuilderTest {
+
+    private BcfToolsCommandBuilder victim;
+
+    @Before
+    public void setup() {
+        victim = new BcfToolsCommandBuilder("input.vcf.gz", "output.vcf.gz");
+    }
+
+    @Test
+    public void testIncludeHardPass() {
+        String bash = victim.includeHardPass().build().asBash();
+        assertThat(bash).contains("/opt/tools/bcftools/1.3.1/bcftools filter -i 'FILTER=\"PASS\"' input.vcf.gz -O z -o output.vcf.gz");
+    }
+
+    @Test
+    public void testPiping() {
+        String bash = victim.includeHardFilter("expression1").includeHardFilter("expression2").includeHardFilter("expression3").build().asBash();
+        assertThat(bash).contains("/opt/tools/bcftools/1.3.1/bcftools filter -i expression1 input.vcf.gz -O u | ");
+        assertThat(bash).contains("| /opt/tools/bcftools/1.3.1/bcftools filter -i expression2 -O u | ");
+        assertThat(bash).contains("| /opt/tools/bcftools/1.3.1/bcftools filter -i expression3 -O z -o output.vcf.gz");
+    }
+
+    @Test
+    public void testExcludeSoftFilter() {
+        String bash = victim.excludeSoftFilter("expression", "FLAG").build().asBash();
+        assertThat(bash).contains("/opt/tools/bcftools/1.3.1/bcftools filter -e expression -s FLAG -m+ input.vcf.gz -O z -o output.vcf.gz");
+    }
+
+    @Test
+    public void testRemoveAnnotation() {
+        String bash = victim.removeAnnotation("FLAG").build().asBash();
+        assertThat(bash).contains("/opt/tools/bcftools/1.3.1/bcftools annotate -x FLAG input.vcf.gz -O z -o output.vcf.gz");
+    }
+
+    @Test
+    public void testAddAnnotation() {
+        String bash = victim.addAnnotation("file", "column").build().asBash();
+        assertThat(bash).contains("/opt/tools/bcftools/1.3.1/bcftools annotate -a file -c column input.vcf.gz -O z -o output.vcf.gz");
+    }
+
+    @Test
+    public void testAddAnnotationWithHeader() {
+        String bash = victim.addAnnotation("file", "column", "header").build().asBash();
+        assertThat(bash).contains("/opt/tools/bcftools/1.3.1/bcftools annotate -a file -h header -c column input.vcf.gz -O z -o output.vcf.gz");
+    }
+
+    @Test
+    public void testSelectSample() {
+        String bash = victim.selectSample("sample").build().asBash();
+        assertThat(bash).contains("/opt/tools/bcftools/1.3.1/bcftools view -s sample input.vcf.gz -O z -o output.vcf.gz");
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testNoCommand() {
+        victim.build();
+    }
+
+    @Test
+    public void testMultipleBuilds() {
+        victim.includeHardFilter("expression1");
+        String bash1 = victim.build().asBash();
+        String bash2 = victim.build().asBash();
+        assertThat(bash1).contains("/opt/tools/bcftools/1.3.1/bcftools filter -i expression1 input.vcf.gz -O z -o output.vcf.gz");
+        assertThat(bash1).isEqualTo(bash2);
+
+        victim.includeHardFilter("expression2");
+        String bash3 = victim.build().asBash();
+        assertThat(bash3).contains("/opt/tools/bcftools/1.3.1/bcftools filter -i expression1 input.vcf.gz -O u | /opt/tools/bcftools/1.3.1/bcftools filter -i expression2 -O z -o output.vcf.gz");
+    }
+
+    @Test
+    public void testIndex() {
+        victim.includeHardPass();
+        List<BashCommand> commands = victim.buildWithIndex();
+        assertThat(commands.get(1).asBash()).contains("/opt/tools/tabix/0.2.6/tabix output.vcf.gz -p vcf");
+    }
+}
