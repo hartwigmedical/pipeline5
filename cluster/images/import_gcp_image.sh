75a7fe8ef8dac022e48995882718b35f89870309
Ned Leitch <N.Leitch@hartwigmedicalfoundation.nl>
2020-03-09 00:12:41 -0400

    Allow project to be given for imaging

--- cluster/images/import_gcp_image.sh
+++ cluster/images/import_gcp_image.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-[[ $# -ne 2 ]] && echo "Provide the pipeline version (eg of the form \"5-x\") and image name (yyyymmddhhmm) of an existing image" && exit 1
+[[ $# -ne 3 ]] && echo "Provide the pipeline version (eg of the form \"5-x\"), image name (yyyymmddhhmm) and destination project" && exit 1
 family="pipeline5-${1}"
 full_name="${family}-${2}"
 
-gcloud compute images create ${full_name} --source-image="${family}-${2}" --source-image-project="hmf-pipeline-development" \
+gcloud compute --project "$3" images create ${full_name} --source-image="${family}-${2}" --source-image-project="hmf-pipeline-development" \
   --storage-location="europe-west4" --family="${family}"
 
