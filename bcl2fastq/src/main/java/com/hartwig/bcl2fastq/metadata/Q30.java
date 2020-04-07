3bf32e4e7752bdc7b5f390e5866b89e52439248d
Paul Wolfe <P.Wolfe@hartwigmedicalfoundation.nl>
2020-04-07 09:48:10 -0400

    DEV-1193 Guard for NaN in the Q30 calculation
    Also adds a small check for when the q30 yield exceeds the yield.

--- bcl2fastq/src/main/java/com/hartwig/bcl2fastq/metadata/Q30.java
+++ bcl2fastq/src/main/java/com/hartwig/bcl2fastq/metadata/Q30.java
@@ -5,6 +5,16 @@ import com.hartwig.bcl2fastq.conversion.WithYieldAndQ30;
 public class Q30 {
 
     public static double of(WithYieldAndQ30 withYieldAndQ30) {
+        if (withYieldAndQ30.yieldQ30() > withYieldAndQ30.yield()) {
+            throw new IllegalArgumentException(String.format(
+                    "Tried to calculate a Q30 where the there was more Q30 yield than total yield. This must be "
+                            + "an issue in the Stats.json which should be investigated. Yield was [%s] Q30 Yeild was [%s]",
+                    withYieldAndQ30.yield(),
+                    withYieldAndQ30.yieldQ30()));
+        }
+        if (withYieldAndQ30.yieldQ30() == 0){
+            return 0d;
+        }
         return withYieldAndQ30.yieldQ30() / (double) withYieldAndQ30.yield() * 100;
     }
 }
