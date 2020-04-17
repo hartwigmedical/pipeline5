40cc95eb132fc42276d398d01173defabf9cc7ca
Jonathan Baber <jonbaber@gmail.com>
2020-04-17 14:02:55 +1000

    DEV-1201: Keep all samples in output

--- cluster/src/main/java/com/hartwig/pipeline/calling/somatic/SageV2PassFilter.java
+++ cluster/src/main/java/com/hartwig/pipeline/calling/somatic/SageV2PassFilter.java
@@ -8,15 +8,13 @@ import com.hartwig.pipeline.execution.vm.BashCommand;
 import com.hartwig.pipeline.execution.vm.OutputFile;
 
 class SageV2PassFilter extends SubStage {
-    private final String tumorName;
 
-    SageV2PassFilter(String tumorName) {
+    SageV2PassFilter() {
         super("sage.pass", OutputFile.GZIPPED_VCF);
-        this.tumorName = tumorName;
     }
 
     @Override
     public List<BashCommand> bash(final OutputFile input, final OutputFile output) {
-        return new BcfToolsCommandListBuilder(input.path(), output.path()).withIndex().includeHardPass().selectSample(tumorName).build();
+        return new BcfToolsCommandListBuilder(input.path(), output.path()).withIndex().includeHardPass().build();
     }
 }
