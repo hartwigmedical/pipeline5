64207a31ca20cf4582a3c59e3aed51bde2b101b3
Jonathan Baber <jonbaber@gmail.com>
2020-04-30 08:40:31 +1000

    DEV-1247: Enable driver catalog with latest PURPLE

--- cluster/src/test/java/com/hartwig/pipeline/tertiary/purple/PurpleTest.java
+++ cluster/src/test/java/com/hartwig/pipeline/tertiary/purple/PurpleTest.java
@@ -58,7 +58,8 @@ public class PurpleTest extends TertiaryStageTest<PurpleOutput> {
                 + "/data/output -amber /data/input/results -cobalt /data/input/results -gc_profile /opt/resources/gc/hg37/GC_profile.1000bp.cnp "
                 + "-somatic_vcf /data/input/tumor.vcf.gz -structural_vcf /data/input/tumor.gridss.filtered.vcf.gz -sv_recovery_vcf "
                 + "/data/input/tumor.gridss.full.vcf.gz -circos /opt/tools/circos/0.69.6/bin/circos -ref_genome "
-                + "/opt/resources/reference_genome/hg37/Homo_sapiens.GRCh37.GATK.illumina.fasta -threads $(grep -c '^processor' /proc/cpuinfo)");
+                + "/opt/resources/reference_genome/hg37/Homo_sapiens.GRCh37.GATK.illumina.fasta -driver_catalog -hotspots "
+                + "/opt/resources/sage/hg37/KnownHotspots.hg19.vcf.gz -threads $(grep -c '^processor' /proc/cpuinfo)");
     }
 
     @Test
