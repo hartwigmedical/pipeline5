c532fa3786bcd9364273cdc3c588e3d4992e647f
Jonathan Baber <jonbaber@gmail.com>
2020-06-10 14:53:34 +1000

    DEV-1201: improved batch pon

--- cluster/src/main/java/com/hartwig/pipeline/calling/somatic/SageCommand.java
+++ cluster/src/main/java/com/hartwig/pipeline/calling/somatic/SageCommand.java
@@ -3,8 +3,8 @@ package com.hartwig.pipeline.calling.somatic;
 import com.hartwig.pipeline.execution.vm.JavaClassCommand;
 import com.hartwig.pipeline.tools.Versions;
 
-class SageCommand extends JavaClassCommand {
-    SageCommand(final String mainClass, final String maxHeapSize, final String... arguments) {
+public class SageCommand extends JavaClassCommand {
+    public SageCommand(final String mainClass, final String maxHeapSize, final String... arguments) {
         super("sage", Versions.SAGE, "sage.jar", mainClass, maxHeapSize, arguments);
     }
 }
