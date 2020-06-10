c5b24eb70005e9f4734a542611cb956cfeeada09
Jonathan Baber <jonbaber@gmail.com>
2020-06-11 08:08:32 +1000

    DEV-1201: Merge in changes

--- cluster/src/test/java/com/hartwig/pipeline/calling/somatic/SageV2CallerTest.java
+++ cluster/src/test/java/com/hartwig/pipeline/calling/somatic/SageV2CallerTest.java
@@ -26,21 +26,19 @@ public class SageV2CallerTest extends TertiaryStageTest<SomaticCallerOutput> {
     @Override
     protected List<String> expectedCommands() {
         return ImmutableList.of("unzip -d /opt/resources /opt/resources/snpeff/hg37/snpEff_v4_3_GRCh37.75.zip",
-                "java -Xmx110G -cp /opt/tools/sage/2.1/sage.jar com.hartwig.hmftools.sage.SageApplication -tumor tumor -tumor_bam /data/input/tumor.bam -reference reference -reference_bam /data/input/reference.bam -hotspots /opt/resources/sage/hg37/KnownHotspots.hg19.vcf.gz -panel_bed /opt/resources/sage/hg37/ActionableCodingPanel.hg19.bed.gz -high_confidence_bed /opt/resources/beds/hg37/NA12878_GIAB_highconf_IllFB-IllGATKHC-CG-Ion-Solid_ALLCHROM_v3.2.2_highconf.bed.gz -ref_genome /opt/resources/reference_genome/hg37/Homo_sapiens.GRCh37.GATK.illumina.fasta -out /data/output/tumor.sage.somatic.vcf.gz -threads $(grep -c '^processor' /proc/cpuinfo)",
-                "(/opt/tools/bcftools/1.9/bcftools filter -i 'FILTER=\"PASS\"' /data/output/tumor.sage.somatic.vcf.gz -O u | /opt/tools/bcftools/1.9/bcftools view -s tumor -O z -o /data/output/tumor.sage.pass.vcf.gz)",
+                "java -Xmx110G -cp /opt/tools/sage/2.1/sage.jar com.hartwig.hmftools.sage.SageApplication -tumor tumor -tumor_bam /data/input/tumor.bam -reference reference -reference_bam /data/input/reference.bam -hotspots /opt/resources/sage/hg37/KnownHotspots.hg19.vcf.gz -panel_bed /opt/resources/sage/hg37/ActionableCodingPanel.hg19.bed.gz -high_confidence_bed /opt/resources/beds/hg37/NA12878_GIAB_highconf_IllFB-IllGATKHC-CG-Ion-Solid_ALLCHROM_v3.2.2_highconf.bed.gz -ref_genome /opt/resources/reference_genome/hg37/Homo_sapiens.GRCh37.GATK.illumina.fasta -out /data/output/tumor.sage.somatic.vcf.gz -assembly hg19 -threads $(grep -c '^processor' /proc/cpuinfo)",
+                "(/opt/tools/bcftools/1.9/bcftools filter -i 'FILTER=\"PASS\"' /data/output/tumor.sage.somatic.vcf.gz -O z -o /data/output/tumor.sage.pass.vcf.gz)",
                 "/opt/tools/tabix/0.2.6/tabix /data/output/tumor.sage.pass.vcf.gz -p vcf",
                 "(/opt/tools/bcftools/1.9/bcftools annotate -a /opt/resources/mappability/hg37/out_150_hg19.mappability.bed.gz -h /opt/resources/mappability/mappability.hdr -c CHROM,FROM,TO,-,MAPPABILITY /data/output/tumor.sage.pass.vcf.gz -O z -o /data/output/tumor.mappability.annotated.vcf.gz)",
                 "/opt/tools/tabix/0.2.6/tabix /data/output/tumor.mappability.annotated.vcf.gz -p vcf",
-                "(/opt/tools/bcftools/1.9/bcftools annotate -a /opt/resources/sage/hg37/SageGermlinePon.hg19.vcf.gz -c PON_COUNT /data/output/tumor.mappability.annotated.vcf.gz -O z -o /data/output/tumor.sage.pon.annotated.vcf.gz)",
+                "(/opt/tools/bcftools/1.9/bcftools annotate -a /opt/resources/sage/hg37/SageGermlinePon.hg19.1000x.vcf.gz -c PON_COUNT,PON_MAX /data/output/tumor.mappability.annotated.vcf.gz -O z -o /data/output/tumor.sage.pon.annotated.vcf.gz)",
                 "/opt/tools/tabix/0.2.6/tabix /data/output/tumor.sage.pon.annotated.vcf.gz -p vcf",
-                "(/opt/tools/bcftools/1.9/bcftools filter -e 'PON_COUNT!= \".\" && (MIN(PON_COUNT) > 9 || (MIN(PON_COUNT) > 2 && INFO/TIER!=\"HOTSPOT\"))' -s SAGE_PON -m+ /data/output/tumor.sage.pon.annotated.vcf.gz -O z -o /data/output/tumor.sage.pon.filter.vcf.gz)",
+                "(/opt/tools/bcftools/1.9/bcftools filter -e 'PON_COUNT!=\".\" && INFO/TIER=\"HOTSPOT\" && PON_MAX>=5 && PON_COUNT >= 10' -s SAGE_PON -m+ /data/output/tumor.sage.pon.annotated.vcf.gz -O u | /opt/tools/bcftools/1.9/bcftools filter -e 'PON_COUNT!=\".\" && INFO/TIER=\"PANEL\" && PON_MAX>=5 && PON_COUNT >= 6' -s SAGE_PON -m+ -O u | /opt/tools/bcftools/1.9/bcftools filter -e 'PON_COUNT!=\".\" && INFO/TIER!=\"HOTSPOT\" && INFO/TIER!=\"PANEL\" && PON_COUNT >= 6' -s SAGE_PON -m+ -O z -o /data/output/tumor.sage.pon.filter.vcf.gz)",
                 "/opt/tools/tabix/0.2.6/tabix /data/output/tumor.sage.pon.filter.vcf.gz -p vcf",
                 "/opt/tools/snpEff/4.3s/snpEff.sh /opt/tools/snpEff/4.3s/snpEff.jar /opt/resources/snpeff/snpEff.config GRCh37.75 /data/output/tumor.sage.pon.filter.vcf.gz /data/output/tumor.snpeff.annotated.vcf",
                 "/opt/tools/tabix/0.2.6/bgzip -f /data/output/tumor.snpeff.annotated.vcf",
                 "/opt/tools/tabix/0.2.6/tabix /data/output/tumor.snpeff.annotated.vcf.gz -p vcf",
-                "java -Xmx8G -cp /opt/tools/sage/2.1/sage.jar com.hartwig.hmftools.sage.SagePostProcessApplication -in /data/output/tumor.snpeff.annotated.vcf.gz -out /data/output/tumor.sage.post.processed.vcf.gz -assembly hg19",
-                "(/opt/tools/bcftools/1.9/bcftools annotate -a /opt/resources/cosmic_v85/CosmicCodingMuts_v85_collapsed.vcf.gz -c ID,INFO /data/output/tumor.sage.post.processed.vcf.gz -O z -o /data/output/tumor.cosmic.annotated.final.vcf.gz)",
-                "/opt/tools/tabix/0.2.6/tabix /data/output/tumor.cosmic.annotated.final.vcf.gz -p vcf");
+                "java -Xmx8G -cp /opt/tools/sage/2.1/sage.jar com.hartwig.hmftools.sage.SagePostProcessApplication -in /data/output/tumor.snpeff.annotated.vcf.gz -out /data/output/tumor.sage.post.processed.final.vcf.gz -assembly hg19");
     }
 
     @Override
