ad9fdf885471875b5a4546673ba170f3499a22a0
Ned Leitch <N.Leitch@hartwigmedicalfoundation.nl>
2020-07-09 09:55:41 -0400

    DEV-1365 Re-enable smoke test

--- cluster/src/test/java/com/hartwig/pipeline/smoke/SmokeTest.java
+++ cluster/src/test/java/com/hartwig/pipeline/smoke/SmokeTest.java
@@ -31,7 +31,6 @@ import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
-@Ignore
 @Category(value = IntegrationTest.class)
 public class SmokeTest {
     private static final String ARCHIVE_BUCKET = "hmf-output-test";
