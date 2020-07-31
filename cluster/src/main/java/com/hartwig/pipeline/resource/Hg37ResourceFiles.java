f202119bca2b6c84c1e2c948507ed926c7b4e150
Charles Shale <shalecharles@gmail.com>
2020-07-31 10:01:17 +1000

    Linx ref files for fragile sites, line elements and origins of replication now ref-genome versioned

--- cluster/src/main/java/com/hartwig/pipeline/resource/Hg37ResourceFiles.java
+++ cluster/src/main/java/com/hartwig/pipeline/resource/Hg37ResourceFiles.java
@@ -12,6 +12,7 @@ import static com.hartwig.pipeline.resource.ResourceNames.MAPPABILITY;
 import static com.hartwig.pipeline.resource.ResourceNames.REFERENCE_GENOME;
 import static com.hartwig.pipeline.resource.ResourceNames.SAGE;
 import static com.hartwig.pipeline.resource.ResourceNames.SNPEFF;
+import static com.hartwig.pipeline.resource.ResourceNames.SV;
 
 import com.hartwig.pipeline.execution.vm.VmDirectories;
 
@@ -88,4 +89,9 @@ public class Hg37ResourceFiles implements ResourceFiles {
     public String bachelorClinvarFilters() { return formPath(BACHELOR, "bachelor_clinvar_filters.csv"); }
 
     public String ensemblDataCache() { return formPath(ENSEMBL, "ensembl_data_cache"); }
+
+    public String fragileSites() { return formPath(SV, "fragile_sites_hmf.csv"); }
+    public String lineElements() { return formPath(SV, "line_elements.csv"); }
+    public String originsOfReplication() { return formPath(SV, "heli_rep_origins.bed"); }
+
 }
--- cluster/src/main/java/com/hartwig/pipeline/resource/ResourceFiles.java
+++ cluster/src/main/java/com/hartwig/pipeline/resource/ResourceFiles.java
@@ -60,6 +60,10 @@ public interface ResourceFiles {
 
     String ensemblDataCache();
 
+    String fragileSites();
+    String lineElements();
+    String originsOfReplication();
+
     String DBNSFP_VCF = ResourceFiles.of(DBNSFP, "dbNSFP2.9.txt.gz");
     String COSMIC_VCF_GZ = ResourceFiles.of(COSMIC, "CosmicCodingMuts_v85_collapsed.vcf.gz");
 }
