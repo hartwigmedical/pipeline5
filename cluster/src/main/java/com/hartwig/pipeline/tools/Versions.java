25bd810f88f852742109add4c84fb803cfbad7d5
Jonathan Baber <jonbaber@gmail.com>
2020-08-13 10:07:06 +1000

    DEV-1455: Bump PURPLE to 2.45

--- cluster/src/main/java/com/hartwig/pipeline/tools/Versions.java
+++ cluster/src/main/java/com/hartwig/pipeline/tools/Versions.java
@@ -20,7 +20,7 @@ public interface Versions {
     String AMBER = "3.4";
     String COBALT = "1.7";
     String HEALTH_CHECKER = "3.1";
-    String PURPLE = "2.43";
+    String PURPLE = "2.45";
     String CIRCOS = "0.69.6";
     String GRIDSS = "2.9.3";
     String GRIPSS = "1.3";
