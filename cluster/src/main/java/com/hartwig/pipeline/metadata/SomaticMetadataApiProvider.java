b9b49bfca3454500596cbb3cb6d9bc2187bf0c12
Ned Leitch <N.Leitch@hartwigmedicalfoundation.nl>
2020-09-09 17:20:19 -0400

    DEV-1488 Fix arg parsing for local JSON
    Replace local JSON factory method with supplier to avoid up-front
    evaluation

--- cluster/src/main/java/com/hartwig/pipeline/metadata/SomaticMetadataApiProvider.java
+++ cluster/src/main/java/com/hartwig/pipeline/metadata/SomaticMetadataApiProvider.java
@@ -2,6 +2,7 @@ package com.hartwig.pipeline.metadata;
 
 import com.google.cloud.storage.Storage;
 import com.hartwig.pipeline.Arguments;
+import com.hartwig.pipeline.alignment.sample.JsonSampleSource;
 import com.hartwig.pipeline.sbpapi.SbpRestApi;
 import com.hartwig.pipeline.transfer.google.GoogleArchiver;
 
@@ -24,6 +25,9 @@ public class SomaticMetadataApiProvider {
                 setId,
                 SbpRestApi.newInstance(arguments.sbpApiUrl()),
                 storage.get(arguments.outputBucket()),
-                new GoogleArchiver(arguments))).orElse(LocalSomaticMetadata.from(arguments));
+                new GoogleArchiver(arguments))).orElseGet(() -> new LocalSomaticMetadata(arguments,
+                arguments.sampleJson()
+                        .map(JsonSampleSource::new)
+                        .orElseThrow(() -> new IllegalArgumentException("Sample JSON must be provided when running in local mode"))));
     }
 }
