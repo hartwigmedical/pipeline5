ebf32aac2f5a222d80d8096ac20a9b67fe9b17fb
Paul Wolfe <P.Wolfe@hartwigmedicalfoundation.nl>
2020-10-12 17:23:47 -0400

    DEV-1525 Missed Sage datatype

--- cluster/src/main/java/com/hartwig/pipeline/calling/somatic/SageCaller.java
+++ cluster/src/main/java/com/hartwig/pipeline/calling/somatic/SageCaller.java
@@ -15,6 +15,7 @@ import com.hartwig.pipeline.execution.vm.OutputFile;
 import com.hartwig.pipeline.execution.vm.VirtualMachineJobDefinition;
 import com.hartwig.pipeline.execution.vm.VmDirectories;
 import com.hartwig.pipeline.execution.vm.unix.UnzipToDirectoryCommand;
+import com.hartwig.pipeline.metadata.AddDatatypeToFile;
 import com.hartwig.pipeline.metadata.SingleSampleRunMetadata;
 import com.hartwig.pipeline.metadata.SomaticRunMetadata;
 import com.hartwig.pipeline.report.Folder;
@@ -40,8 +41,7 @@ public class SageCaller extends TertiaryStage<SomaticCallerOutput> {
     private OutputFile filteredOutputFile;
     private OutputFile unfilteredOutputFile;
 
-    public SageCaller(final AlignmentPair alignmentPair, final ResourceFiles resourceFiles,
-            final PersistedDataset persistedDataset) {
+    public SageCaller(final AlignmentPair alignmentPair, final ResourceFiles resourceFiles, final PersistedDataset persistedDataset) {
         super(alignmentPair);
         this.resourceFiles = resourceFiles;
         this.persistedDataset = persistedDataset;
@@ -95,6 +95,11 @@ public class SageCaller extends TertiaryStage<SomaticCallerOutput> {
                 .addReportComponents(vcfComponent(filteredOutputFile.fileName(), bucket, resultsDirectory))
                 .addReportComponents(new RunLogComponent(bucket, NAMESPACE, Folder.root(), resultsDirectory))
                 .addReportComponents(new StartupScriptComponent(bucket, NAMESPACE, Folder.root()))
+                .addFurtherOperations(new AddDatatypeToFile(DataType.SOMATIC_VARIANTS,
+                        Folder.root(),
+                        namespace(),
+                        filteredOutputFile.fileName(),
+                        metadata.barcode()))
                 .build();
     }
 
