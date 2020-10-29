12e5de38de3fc00fc3a9c24beb1d43051d56e102
Paul Wolfe <P.Wolfe@hartwigmedicalfoundation.nl>
2020-10-29 09:21:31 -0400

    DEV-1599 Downgrade gridss to 2.9.4
    2.10.1 was not a version only bump.

--- cluster/src/main/java/com/hartwig/pipeline/tools/Versions.java
+++ cluster/src/main/java/com/hartwig/pipeline/tools/Versions.java
@@ -22,7 +22,7 @@ public interface Versions {
     String HEALTH_CHECKER = "3.2";
     String PURPLE = "2.51";
     String CIRCOS = "0.69.6";
-    String GRIDSS = "2.10.1";
+    String GRIDSS = "2.9.4";
     String GRIPSS = "1.8";
     String LINX = "1.11";
     String BACHELOR = "1.12";
