74e8c823224c35512bc5db72cdd9f489234dcb5e
Paul Wolfe <P.Wolfe@hartwigmedicalfoundation.nl>
2021-01-18 14:52:17 -0500

    DEV-1762 Publish failures rather than results
    The API has changed to accept a failure instead of a result, which looks almost the
    same except will be null/empty when the pipeline completes successfully.
    
    The SnpCheck status is removed and all runs now finish with the "Finished" status.

--- cluster/src/test/java/com/hartwig/pipeline/sbpapi/SbpRunFailureTest.java
+++ cluster/src/test/java/com/hartwig/pipeline/sbpapi/SbpRunFailureTest.java
@@ -6,31 +6,24 @@ import com.hartwig.pipeline.execution.PipelineStatus;
 
 import org.junit.Test;
 
-public class SbpRunResultTest {
+public class SbpRunFailureTest {
 
     @Test
     public void failureMapsToPipelineTechnicalFailure() {
-        SbpRunResult victim = SbpRunResult.from(PipelineStatus.FAILED);
+        SbpRunFailure victim = SbpRunFailure.from(PipelineStatus.FAILED);
         assertThat(victim.category()).isEqualTo("TechnicalFailure");
         assertThat(victim.type()).isEqualTo("Pipeline");
     }
 
     @Test
     public void qcFailureMapsToHealthCheckerQCFailure() {
-        SbpRunResult victim = SbpRunResult.from(PipelineStatus.QC_FAILED);
+        SbpRunFailure victim = SbpRunFailure.from(PipelineStatus.QC_FAILED);
         assertThat(victim.category()).isEqualTo("QCFailure");
         assertThat(victim.type()).isEqualTo("HealthCheck");
     }
 
-    @Test
-    public void successMapsToSuccess() {
-        SbpRunResult victim = SbpRunResult.from(PipelineStatus.SUCCESS);
-        assertThat(victim.category()).isEqualTo("Success");
-        assertThat(victim.type()).isEqualTo("Success");
-    }
-
     @Test(expected = IllegalArgumentException.class)
     public void unmappableStatusThrowsIllegalArgument() {
-        SbpRunResult.from(PipelineStatus.PERSISTED);
+        SbpRunFailure.from(PipelineStatus.PERSISTED);
     }
 }
