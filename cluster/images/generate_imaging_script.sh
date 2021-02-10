43d28b383965444aa2653f08e5d03af9d5021c8f
Paul Wolfe <P.Wolfe@hartwigmedicalfoundation.nl>
2021-02-10 11:15:30 -0500

    Bump to 5.20

--- cluster/images/generate_imaging_script.sh
+++ cluster/images/generate_imaging_script.sh
@@ -5,7 +5,7 @@
 LOCATION="europe-west4"
 ZONE="${LOCATION}-a"
 PROJECT="hmf-pipeline-development"
-VERSION=5-19
+VERSION=5-20
 
 TOOLS_ONLY=false
 while getopts ':tf:' flag; do
--- cluster/src/main/java/com/hartwig/pipeline/tools/Versions.java
+++ cluster/src/main/java/com/hartwig/pipeline/tools/Versions.java
@@ -70,6 +70,6 @@ public interface Versions {
     }
 
     static String imageVersion() {
-        return "5-19";
+        return "5-20";
     }
 }
