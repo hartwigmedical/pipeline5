444c54c0a9b1a3d92f9159199a2cb9fae0f64c9a
Stef van Lieshout <stefvanlieshout@fastmail.fm>
2021-04-01 18:25:23 +0200

    DEV-1679: framework for VirusBreakend finished

--- cluster/src/main/java/com/hartwig/pipeline/tertiary/virusbreakend/VirusBreakendCommand.java
+++ cluster/src/main/java/com/hartwig/pipeline/tertiary/virusbreakend/VirusBreakendCommand.java
@@ -11,45 +11,45 @@ import com.hartwig.pipeline.execution.vm.VmDirectories;
 import com.hartwig.pipeline.resource.ResourceFiles;
 import com.hartwig.pipeline.tools.Versions;
 
-public class VirusBreakendCommand {
+public class VirusBreakendCommand implements BashCommand {
 
     private static final String GRIDSS = "gridss";
     private final ResourceFiles resourceFiles;
     private final String tumorSample;
     private final String tumorBamPath;
+    private final String bash;
 
     public VirusBreakendCommand(ResourceFiles resourceFiles, String tumorSample, String tumorBamPath) {
         this.resourceFiles = resourceFiles;
         this.tumorBamPath = tumorBamPath;
         this.tumorSample = tumorSample;
+        this.bash = new VersionedToolCommand(GRIDSS,
+                "virusbreakend.sh",
+                Versions.GRIDSS_VIRUS_BREAKEND
+//                "-o",
+//                output.path(),
+//                "-w",
+//                VmDirectories.OUTPUT,
+//                "-r",
+//                resourceFiles.refGenomeFile(),
+//                "-j",
+//                VmDirectories.TOOLS + "/" + GRIDSS + "/" + Versions.GRIDSS_VIRUS_BREAKEND + "/gridss.jar",
+//                "-b",
+//                resourceFiles.gridssBlacklistBed(),
+//                "-c",
+//                resourceFiles.gridssPropertiesFile(),
+//                "--repeatmaskerbed",
+//                resourceFiles.gridssRepeatMaskerDbBed(),
+//                "--jvmheap",
+//                "31G",
+//                "--labels",
+//                tumorSample,
+//                tumorBamPath
+        ).asBash();
     }
 
     @Override
-    public List<BashCommand> bash(final OutputFile input, final OutputFile output) {
-        BashCommand virusbreakend = new VersionedToolCommand(GRIDSS,
-                "virusbreakend.sh",
-                Versions.VIRUS_BREAKEND,
-                "-o",
-                output.path(),
-                "-w",
-                VmDirectories.OUTPUT,
-                "-r",
-                resourceFiles.refGenomeFile(),
-                "-j",
-                VmDirectories.TOOLS + "/" + GRIDSS + "/" + Versions.VIRUS_BREAKEND + "/gridss.jar",
-                "-b",
-                resourceFiles.gridssBlacklistBed(),
-                "-c",
-                resourceFiles.gridssPropertiesFile(),
-                "--repeatmaskerbed",
-                resourceFiles.gridssRepeatMaskerDbBed(),
-                "--jvmheap",
-                "31G",
-                "--labels",
-                tumorSample,
-                tumorBamPath);
-
-        BashCommand index = new TabixCommand(output.path());
-        return Lists.newArrayList(virusbreakend, index);
+    public String asBash() {
+        return bash;
     }
 }
