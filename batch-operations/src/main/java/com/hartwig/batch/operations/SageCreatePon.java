a1f15075a8c94519103521898abc14f5472cf2c4
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2021-04-01 10:03:31 +0200

    Improve error message when running SAGE in pon mode

--- batch-operations/src/main/java/com/hartwig/batch/operations/SageCreatePon.java
+++ batch-operations/src/main/java/com/hartwig/batch/operations/SageCreatePon.java
@@ -22,6 +22,7 @@ import com.hartwig.pipeline.storage.GoogleStorageLocation;
 import com.hartwig.pipeline.storage.RuntimeBucket;
 
 public class SageCreatePon implements BatchOperation {
+
     @Override
     public VirtualMachineJobDefinition execute(final InputBundle inputs, final RuntimeBucket runtimeBucket,
             final BashStartupScript startupScript, final RuntimeFiles executionFlags) {
@@ -41,7 +42,7 @@ public class SageCreatePon implements BatchOperation {
 //                "gs://batch-sage-validation/resources/sage.jar",
 //                "/opt/tools/sage/" + Versions.SAGE + "/sage.jar"));
 
-        // Download germline VCFS (and indexes)
+        // Download germline VCFs (and indexes)
         startupScript.addCommand(() -> format("gsutil -u hmf-crunch -m cp %s %s",
                 "gs://batch-sage/*/sage/*.sage.somatic.vcf.gz",
                 VmDirectories.INPUT));
