dd99a7c561b0612efc05be20f97b10f198bbcff4
Stef van Lieshout <stefvanlieshout@fastmail.fm>
2021-04-08 13:55:03 +0200

    DEV-1679: ongoing VirusBreakend implementation

--- cluster/src/main/java/com/hartwig/pipeline/tertiary/virusbreakend/VirusBreakendCommand.java
+++ cluster/src/main/java/com/hartwig/pipeline/tertiary/virusbreakend/VirusBreakendCommand.java
@@ -1,50 +1,32 @@
 package com.hartwig.pipeline.tertiary.virusbreakend;
 
-import java.util.List;
-
-import com.google.common.collect.Lists;
-import com.hartwig.pipeline.calling.command.TabixCommand;
 import com.hartwig.pipeline.calling.command.VersionedToolCommand;
 import com.hartwig.pipeline.execution.vm.BashCommand;
 import com.hartwig.pipeline.execution.vm.OutputFile;
 import com.hartwig.pipeline.execution.vm.VmDirectories;
 import com.hartwig.pipeline.resource.ResourceFiles;
 import com.hartwig.pipeline.tools.Versions;
+import static com.hartwig.pipeline.resource.ResourceNames.VIRUSBREAKEND_DB;
 
 public class VirusBreakendCommand implements BashCommand {
 
-    private static final String GRIDSS = "gridss";
-    private final ResourceFiles resourceFiles;
-    private final String tumorSample;
-    private final String tumorBamPath;
     private final String bash;
 
     public VirusBreakendCommand(ResourceFiles resourceFiles, String tumorSample, String tumorBamPath) {
-        this.resourceFiles = resourceFiles;
-        this.tumorBamPath = tumorBamPath;
-        this.tumorSample = tumorSample;
-        this.bash = new VersionedToolCommand(GRIDSS,
+        this.bash = new VersionedToolCommand("gridss",
                 "virusbreakend.sh",
-                Versions.GRIDSS_VIRUS_BREAKEND
-//                "-o",
-//                output.path(),
-//                "-w",
-//                VmDirectories.OUTPUT,
-//                "-r",
-//                resourceFiles.refGenomeFile(),
-//                "-j",
-//                VmDirectories.TOOLS + "/" + GRIDSS + "/" + Versions.GRIDSS_VIRUS_BREAKEND + "/gridss.jar",
-//                "-b",
-//                resourceFiles.gridssBlacklistBed(),
-//                "-c",
-//                resourceFiles.gridssPropertiesFile(),
-//                "--repeatmaskerbed",
-//                resourceFiles.gridssRepeatMaskerDbBed(),
-//                "--jvmheap",
-//                "31G",
-//                "--labels",
-//                tumorSample,
-//                tumorBamPath
+                Versions.GRIDSS_VIRUS_BREAKEND,
+                "-o",
+                tumorSample + ".virusbreakend.vcf",
+                "-w",
+                VmDirectories.OUTPUT,
+                "-r",
+                resourceFiles.refGenomeFile(),
+                "--db",
+                VmDirectories.RESOURCES + "/virusbreakend/" + VIRUSBREAKEND_DB,
+                "-j",
+                VmDirectories.TOOLS + "/gridss/" + Versions.GRIDSS_VIRUS_BREAKEND + "/gridss.jar",
+                tumorBamPath
         ).asBash();
     }
 
