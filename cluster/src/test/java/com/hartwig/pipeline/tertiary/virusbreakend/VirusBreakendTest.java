a10416b42cf54b75de2abdf1caf2add9ce2dab10
Stef van Lieshout <stefvanlieshout@fastmail.fm>
2021-04-15 10:21:59 +0200

    DEV-1679: added tests

---
+++ cluster/src/test/java/com/hartwig/pipeline/tertiary/virusbreakend/VirusBreakendTest.java
@@ -0,0 +1,80 @@
+package com.hartwig.pipeline.tertiary.virusbreakend;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import com.hartwig.pipeline.datatypes.DataType;
+import com.hartwig.pipeline.metadata.AddDatatype;
+import com.hartwig.pipeline.metadata.ArchivePath;
+import com.hartwig.pipeline.metadata.SomaticRunMetadata;
+import com.hartwig.pipeline.report.Folder;
+import com.hartwig.pipeline.stages.Stage;
+import com.hartwig.pipeline.storage.GoogleStorageLocation;
+import com.hartwig.pipeline.tertiary.TertiaryStageTest;
+import com.hartwig.pipeline.testsupport.TestInputs;
+
+import org.junit.Before;
+
+public class VirusBreakendTest extends TertiaryStageTest<VirusBreakendOutput> {
+
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected Stage<VirusBreakendOutput, SomaticRunMetadata> createVictim() {
+        return new VirusBreakend(TestInputs.defaultPair(), TestInputs.REF_GENOME_37_RESOURCE_FILES, persistedDataset);
+    }
+
+    @Override
+    protected List<AddDatatype> expectedFurtherOperations() {
+        return List.of(new AddDatatype(DataType.VIRUSBREAKEND_VARIANTS,
+                        TestInputs.defaultSomaticRunMetadata().barcode(),
+                        new ArchivePath(Folder.root(), VirusBreakend.NAMESPACE, "tumor.virusbreakend.vcf")),
+                new AddDatatype(DataType.VIRUSBREAKEND_SUMMARY,
+                        TestInputs.defaultSomaticRunMetadata().barcode(),
+                        new ArchivePath(Folder.root(), VirusBreakend.NAMESPACE, "tumor.virusbreakend.summary.tsv")));
+    }
+
+    @Override
+    protected void validateOutput(final VirusBreakendOutput output) {
+        assertThat(output.outputDirectory().bucket()).isEqualTo("run-reference-tumor-test/virusbreakend");
+        assertThat(output.outputDirectory().path()).isEqualTo("results");
+        assertThat(output.outputDirectory().isDirectory()).isTrue();
+    }
+
+    @Override
+    protected void validatePersistedOutput(final VirusBreakendOutput output) {
+        assertThat(output.outputDirectory()).isEqualTo(GoogleStorageLocation.of(OUTPUT_BUCKET, "set/virusbreakend", true));
+    }
+
+    @Override
+    protected void setupPersistedDataset() {
+        persistedDataset.addPath(DataType.VIRUSBREAKEND_SUMMARY, "virusbreakend");
+    }
+
+    @Override
+    protected void validatePersistedOutputFromPersistedDataset(final VirusBreakendOutput output) {
+        assertThat(output.outputDirectory()).isEqualTo(GoogleStorageLocation.of(OUTPUT_BUCKET, "virusbreakend", true));
+    }
+
+    @Override
+    protected List<String> expectedCommands() {
+        return List.of("export PATH=/opt/tools/gridss/2.11.1:$PATH",
+                "export PATH=/opt/tools/repeatmasker/4.1.1:$PATH",
+                "export PATH=/opt/tools/kraken2/2.1.0:$PATH",
+                "export PATH=/opt/tools/samtools/1.10:$PATH",
+                "export PATH=/opt/tools/bcftools/1.9:$PATH",
+                "export PATH=/opt/tools/bwa/0.7.17:$PATH",
+                "/opt/tools/gridss/2.11.1/virusbreakend.sh "
+                        + "--output /data/output/tumor.virusbreakend.vcf "
+                        + "--workingdir /data/output "
+                        + "--reference /opt/resources/reference_genome/37/Homo_sapiens.GRCh37.GATK.illumina.fasta "
+                        + "--db /opt/resources/virusbreakend/virusbreakend_db "
+                        + "--jar /opt/tools/gridss/2.11.1/gridss.jar "
+                        + "/data/input/tumor.bam"
+        );
+    }
+}
