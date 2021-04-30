6c9035f3ff09e72de7c0aa4e2801db3472c9a322
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2021-04-30 13:40:22 +0200

    Update to correct ref genome config for LINX and add todo for updating SAGE once new SAGE is released

--- cluster/src/main/java/com/hartwig/pipeline/resource/RefGenomeVersion.java
+++ cluster/src/main/java/com/hartwig/pipeline/resource/RefGenomeVersion.java
@@ -1,8 +1,9 @@
 package com.hartwig.pipeline.resource;
 
 public enum RefGenomeVersion {
-    V37("hg19", "HG37", "37", "37", "HG19"),
-    V38("hg38", "HG38", "38", "38", "HG38");
+    // TODO SAGE from v2.9 onwards will expect 37/38
+    V37("hg19", "37", "37", "37", "HG19"),
+    V38("hg38", "38", "38", "38", "HG38");
 
     private final String sage;
     private final String linx;
