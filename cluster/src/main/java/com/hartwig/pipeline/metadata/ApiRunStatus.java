e80e51226a46682f08e2290e612b0a61b86b406c
pauldwolfe <p.wolfe@hartwigmedicalfoundation.nl>
2021-10-15 16:04:35 -0400

    DEV-2227

--- cluster/src/main/java/com/hartwig/pipeline/metadata/ApiRunStatus.java
+++ cluster/src/main/java/com/hartwig/pipeline/metadata/ApiRunStatus.java
@@ -4,32 +4,20 @@ import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.hartwig.api.RunApi;
 import com.hartwig.api.model.Run;
 import com.hartwig.api.model.RunFailure;
 import com.hartwig.api.model.Status;
 import com.hartwig.api.model.UpdateRun;
 import com.hartwig.pipeline.execution.PipelineStatus;
-import com.hartwig.pipeline.jackson.ObjectMappers;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class ApiRunStatus {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(ApiRunStatus.class);
     private static final String PIPELINE_SOURCE = "Pipeline";
     private static final String HEALTH_CHECK = "HealthCheck";
 
     static void start(final RunApi runApi, final Run run) {
-        UpdateRun update = new UpdateRun().failure(null).status(Status.PROCESSING).startTime(timestamp());
-        try {
-            LOGGER.info("Updating API with run status and result [{}]", ObjectMappers.get().writeValueAsString(update));
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-        runApi.update(run.getId(), update);
+        runApi.update(run.getId(), new UpdateRun().failure(null).status(Status.PROCESSING).startTime(timestamp()));
     }
 
     static void finish(final RunApi runApi, final Run run, final PipelineStatus status) {
