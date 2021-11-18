a29101e2704f9fc2e0c591db83128aea1b651e58
pauldwolfe <p.wolfe@hartwigmedicalfoundation.nl>
2021-11-18 11:07:47 -0500

    DEV-2342 Upgrade PROTECT to 1.5

--- cluster/src/main/java/com/hartwig/pipeline/tools/Versions.java
+++ cluster/src/main/java/com/hartwig/pipeline/tools/Versions.java
@@ -30,7 +30,7 @@ public interface Versions {
     String CHORD = "2.00_1.14";
     String SAMTOOLS = "1.10";
     String BAMCOMP = "1.3";
-    String PROTECT = "1.4";
+    String PROTECT = "1.5";
     String REPEAT_MASKER = "4.1.1";
     String KRAKEN = "2.1.0";
     String CUPPA = "1.5";
