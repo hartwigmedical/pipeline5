809c47b80e147c9b47299c89cebd73c3675bc90f
pwolfe <p.wolfe@hartwigmedicalfoundation.nl>
2022-01-03 10:33:27 -0500

    DEV-2464 Revert this change
    Does not improve reproduceability as we already use constant
    threads

--- cluster/src/main/java/com/hartwig/pipeline/alignment/bwa/BwaMemCommand.java
+++ cluster/src/main/java/com/hartwig/pipeline/alignment/bwa/BwaMemCommand.java
@@ -7,9 +7,6 @@ import com.hartwig.pipeline.execution.vm.Bash;
 
 class BwaMemCommand extends BwaCommand {
 
-    private static final String MAGIC_NUMBER_BATCH_SIZE = "10000000";
-    private static final String USE_DETERMINISTIC_BATCH_SIZE = "-K";
-
     BwaMemCommand(final String recordGroupId, final String sampleName, final String flowcellId, final String referenceGenomePath,
             final String first, final String second) {
         super("mem",
@@ -18,8 +15,8 @@ class BwaMemCommand extends BwaCommand {
                 "-Y",
                 "-t",
                 Bash.allCpus(),
-                USE_DETERMINISTIC_BATCH_SIZE,
-                MAGIC_NUMBER_BATCH_SIZE,
+                "-K",
+                "10000000",
                 referenceGenomePath,
                 first,
                 second);
