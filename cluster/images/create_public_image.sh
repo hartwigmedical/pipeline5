d32c2328ad80e2e78e50c3a269bc6cc68c7e7ba3
Ned Leitch <ned.leitch@gmail.com>
2022-02-14 17:22:37 -0500

    DEV-1933 Revert unrelated imager changes

--- cluster/images/create_public_image.sh
+++ cluster/images/create_public_image.sh
@@ -13,17 +13,16 @@ while getopts ':tf:' flag; do
         *) ;;
     esac
 done
-image_family="pipeline5-${VERSION}${FLAVOUR+"-$FLAVOUR"}"
-source_instance="${image_family}-$(whoami)"
-imageName="${image_family}-$(date +%Y%m%d%H%M)"
-source_project="debian-cloud"
-source_family="debian-9"
+sourceInstance="pipeline5-${VERSION}${FLAVOUR+"-$FLAVOUR"}"
+imageName="${sourceInstance}-$(date +%Y%m%d%H%M)"
+image_project="debian-cloud"
+image_family="debian-9"
 base_image_cmds="$(dirname "$0")/base.cmds"
 tools_image_cmds="$(dirname "$0")/tools.cmds"
 all_cmds=$(echo $base_image_cmds $tools_image_cmds)
 if [ "$TOOLS_ONLY" = true ]; then
-  source_project="hmf-pipeline-development"
-  source_family=${image_family}
+  image_project="hmf-pipeline-development"
+  image_family=${sourceInstance}
   all_cmds=$tools_image_cmds
 fi
 
@@ -37,29 +36,29 @@ generated_script=$(mktemp -t image_script_generated_XXXXX.sh)
 echo "#!/usr/bin/env bash"
 echo
 echo "set -e"
-echo $GCL instances create $source_instance --description=\"Pipeline5 disk imager started $(date) by $(whoami)\" --zone=${ZONE} \
-    --boot-disk-size 200 --boot-disk-type pd-ssd --machine-type n1-highcpu-4 --image-project=${source_project} \
-    --image-family=${source_family} --scopes=default,cloud-source-repos-ro
+echo $GCL instances create $sourceInstance --description=\"Instance for pipeline5 disk image creation\" --zone=${ZONE} \
+    --boot-disk-size 200 --boot-disk-type pd-ssd --machine-type n1-highcpu-4 --image-project=${image_project} \
+    --image-family=${image_family} --scopes=default,cloud-source-repos-ro
 echo sleep 10
-echo "$GCL scp $(dirname $0)/mk_python_venv ${source_instance}:/tmp/ --zone=${ZONE}"
-echo "$GCL scp $(dirname $0)/jranke.asc ${source_instance}:/tmp/ --zone=${ZONE}"
+echo "$GCL scp $(dirname $0)/mk_python_venv ${sourceInstance}:/tmp/ --zone=${ZONE}"
+echo "$GCL scp $(dirname $0)/jranke.asc ${sourceInstance}:/tmp/ --zone=${ZONE}"
 cat $all_cmds | egrep -v  '^#|^ *$' | while read cmd
 do
-    echo "$GCL ssh $source_instance --zone=${ZONE} --command=\"$cmd\""
+    echo "$GCL ssh $sourceInstance --zone=${ZONE} --command=\"$cmd\""
 done
 
 if [ -n "$FLAVOUR" ]; then
     bucket="gs://common-resources-${FLAVOUR}-overrides"
     copy_overrides="sudo gsutil -m -o 'GSUtil:parallel_thread_count=1' -o 'GSUtil:sliced_object_download_max_components=4' cp -r ${bucket}/* /opt/resources/"
-    echo "$GCL ssh ${source_instance} --zone=${ZONE} --command=\"$copy_overrides\""
+    echo "$GCL ssh ${sourceInstance} --zone=${ZONE} --command=\"$copy_overrides\""
 fi
 
-echo "$GCL ssh $source_instance --zone=${ZONE} --command=\"cd /opt/resources && sudo git tag ${imageName} && git push origin ${imageName}\""
-echo "$GCL ssh $source_instance --zone=${ZONE} --command=\"sudo rm -r /opt/resources/.git\""
+echo "$GCL ssh $sourceInstance --zone=${ZONE} --command=\"cd /opt/resources && sudo git tag ${imageName} && git push origin ${imageName}\""
+echo "$GCL ssh $sourceInstance --zone=${ZONE} --command=\"sudo rm -r /opt/resources/.git\""
 
-echo "$GCL instances stop ${source_instance} --zone=${ZONE}"
-echo "$GCL images create ${imageName} --family=${image_family} --source-disk=${source_instance} --source-disk-zone=${ZONE} --storage-location=${LOCATION}"
-echo "$GCL instances -q delete ${source_instance} --zone=${ZONE}"
+echo "$GCL instances stop ${sourceInstance} --zone=${ZONE}"
+echo "$GCL images create ${imageName} --family=${sourceInstance} --source-disk=${sourceInstance} --source-disk-zone=${ZONE} --storage-location=${LOCATION}"
+echo "$GCL instances -q delete ${sourceInstance} --zone=${ZONE}"
 ) > $generated_script
 chmod +x $generated_script
 $generated_script
