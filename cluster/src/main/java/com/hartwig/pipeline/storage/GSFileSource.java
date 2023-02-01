2eae445d4aec940751e55bc1ac5826e37c88628c
pwolfe <p.wolfe@hartwigmedicalfoundation.nl>
2023-02-01 16:52:03 -0500

    DEV-3068 Accept PDL with both gs:// and no gs urls

--- cluster/src/main/java/com/hartwig/pipeline/storage/GSFileSource.java
+++ cluster/src/main/java/com/hartwig/pipeline/storage/GSFileSource.java
@@ -4,8 +4,10 @@ import java.util.function.Function;
 
 public class GSFileSource implements Function<String, String> {
 
+    private static final String GS = "gs://";
+
     @Override
     public String apply(final String file) {
-        return String.format("gs://%s", file);
+        return file.startsWith(GS) ? file : String.format("%s%s", GS, file);
     }
 }
