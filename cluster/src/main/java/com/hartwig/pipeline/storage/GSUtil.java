0c51befe8834e96288efcc699c8f9561f05db302
Ned Leitch <ned.leitch@gmail.com>
2023-10-24 11:49:57 -0400

    DEV-3595 Remove cruft from GSUtil class
    Some cruft and some code recently made incorrect by the removal of the
    key-handling from the command line. Still don't expect this to fully
    work but want to see what breaks in the SmokeTest environment.

--- cluster/src/main/java/com/hartwig/pipeline/storage/GSUtil.java
+++ cluster/src/main/java/com/hartwig/pipeline/storage/GSUtil.java
@@ -7,32 +7,19 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class GSUtil {
-
-    private static boolean VERBOSE = false;
-    private static int TIMEOUT_HOURS;
-
-    public static void configure(final boolean verbose, final int timeoutHours) {
-        VERBOSE = verbose;
-        TIMEOUT_HOURS = timeoutHours;
-    }
-
-    public static void auth(final String gsdkPath, final String keyFile) throws IOException, InterruptedException {
-        ProcessBuilder processBuilder =
-                new ProcessBuilder(gsdkPath + "/gcloud", "auth", "activate-service-account", String.format("--key-file=%s", keyFile));
-        Processes.run(processBuilder, VERBOSE);
-    }
+    private static final boolean VERBOSE = false;
 
     public static void cp(final String gsdkPath, final String sourceUrl, final String targetUrl) throws IOException, InterruptedException {
         cp(gsdkPath, sourceUrl, targetUrl, null, false);
     }
 
-    public static void cp(final String gsdkPath, final String sourceUrl, final String targetUrl, final String userProject, final String... switches)
-            throws IOException, InterruptedException {
+    public static void cp(final String gsdkPath, final String sourceUrl, final String targetUrl, final String userProject,
+            final String... switches) throws IOException, InterruptedException {
         cp(gsdkPath, sourceUrl, targetUrl, userProject, false, switches);
     }
 
-    public static void cp(final String gsdkPath, final String sourceUrl, final String targetUrl, final String userProject, final boolean recurse, final String... switches)
-            throws IOException, InterruptedException {
+    public static void cp(final String gsdkPath, final String sourceUrl, final String targetUrl, final String userProject,
+            final boolean recurse, final String... switches) throws IOException, InterruptedException {
         List<String> command = new ArrayList<>();
         command.add(gsutil(gsdkPath));
         if (userProject != null) {
@@ -48,35 +35,7 @@ public class GSUtil {
         command.add(sourceUrl);
         command.add(targetUrl);
         ProcessBuilder processBuilder = new ProcessBuilder(command).inheritIO();
-        Processes.run(processBuilder, VERBOSE, TIMEOUT_HOURS, TimeUnit.HOURS);
-    }
-
-    public static void rsync(final String gsdkPath, final String sourceUrl, final String targetUrl, final String userProject, final boolean recurse)
-            throws IOException, InterruptedException {
-        rsync(gsdkPath, sourceUrl, targetUrl, userProject, null, recurse);
-    }
-
-    public static void rsync(final String gsdkPath, final String sourceUrl, final String targetUrl, final String userProject, final String exclusions, final boolean recurse)
-            throws IOException, InterruptedException {
-        List<String> command = new ArrayList<>();
-        command.add(gsutil(gsdkPath));
-        if (userProject != null) {
-            command.add("-u");
-            command.add(userProject);
-        }
-        command.add("-qm");
-        command.add("rsync");
-        if (exclusions != null) {
-            command.add("-x");
-            command.add(exclusions);
-        }
-        if (recurse) {
-            command.add("-r");
-        }
-        command.add(sourceUrl);
-        command.add(targetUrl);
-        ProcessBuilder processBuilder = new ProcessBuilder(command).inheritIO();
-        Processes.run(processBuilder, VERBOSE, TIMEOUT_HOURS, TimeUnit.HOURS);
+        Processes.run(processBuilder, VERBOSE, 0, TimeUnit.HOURS);
     }
 
     public static void rm(final String gsdkPath, final String path) {
@@ -88,7 +47,7 @@ public class GSUtil {
         command.add("gs://" + path);
         ProcessBuilder processBuilder = new ProcessBuilder(command).inheritIO();
         try {
-            Processes.run(processBuilder, VERBOSE, TIMEOUT_HOURS, TimeUnit.HOURS);
+            Processes.run(processBuilder, VERBOSE, 0, TimeUnit.HOURS);
         } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
