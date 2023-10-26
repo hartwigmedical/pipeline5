aab79da19a28aa7eac004aaceef94593c967e194
Khalid Zuberi <k.zuberi@hartwigmedicalfoundation.nl>
2023-10-26 13:29:41 -0400

    DEV-3663: add -experiment_type to OrangeTest

--- cluster/src/test/java/com/hartwig/pipeline/tertiary/orange/OrangeTest.java
+++ cluster/src/test/java/com/hartwig/pipeline/tertiary/orange/OrangeTest.java
@@ -121,7 +121,7 @@ public class OrangeTest extends TertiaryStageTest<OrangeOutput> {
 
     @Override
     protected List<String> expectedCommands() {
-        String jarRunCommand = toolCommand(ORANGE) + " -output_dir /data/output " + "-ref_genome_version 37 " + "-tumor_sample_id tumor "
+        String jarRunCommand = toolCommand(ORANGE) + " -output_dir /data/output " + "-experiment_type WGS " + "-ref_genome_version 37 " + "-tumor_sample_id tumor "
                 + "-reference_sample_id reference " + "-doid_json /opt/resources/disease_ontology/doid.json "
                 + "-primary_tumor_doids \"01;02\" " + "-ref_sample_wgs_metrics_file /data/input/reference.wgsmetrics "
                 + "-tumor_sample_wgs_metrics_file /data/input/tumor.wgsmetrics "
