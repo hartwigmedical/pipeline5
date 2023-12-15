4782741937a9129321ea090523e27a2736adf3a1
KasperWolsink99 <k.wolsink@hartwigmedicalfoundation.nl>
2023-12-15 13:08:28 +0100

    DEV-3490 update cloudbuild and disable smoketest temporarily

--- cluster/src/test/java/com/hartwig/pipeline/smoke/SmokeTest.java
+++ cluster/src/test/java/com/hartwig/pipeline/smoke/SmokeTest.java
@@ -41,6 +41,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Parallelized.class)
 @Category(value = IntegrationTest.class)
+@Ignore
 public class SmokeTest {
 
     protected static final String FILE_ENCODING = "UTF-8";
