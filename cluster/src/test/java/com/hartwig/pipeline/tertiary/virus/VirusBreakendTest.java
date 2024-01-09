99113a7a7ed3db9bf61575d4b82139f5e4e35305
KasperWolsink99 <k.wolsink@hartwigmedicalfoundation.nl>
2024-01-09 13:05:36 +0100

    INFRA-158 fix VirusBreakendTest

--- cluster/src/test/java/com/hartwig/pipeline/tertiary/virus/VirusBreakendTest.java
+++ cluster/src/test/java/com/hartwig/pipeline/tertiary/virus/VirusBreakendTest.java
@@ -1,6 +1,8 @@
 package com.hartwig.pipeline.tertiary.virus;
 
 import static com.hartwig.pipeline.testsupport.TestInputs.SOMATIC_BUCKET;
+import static com.hartwig.pipeline.tools.HmfTool.GRIDSS;
+import static com.hartwig.pipeline.tools.HmfTool.VIRUSBREAKEND_GRIDSS;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,6 +19,7 @@ import com.hartwig.pipeline.output.Folder;
 import com.hartwig.pipeline.stages.Stage;
 import com.hartwig.pipeline.tertiary.TertiaryStageTest;
 import com.hartwig.pipeline.testsupport.TestInputs;
+import com.hartwig.pipeline.tools.HmfTool;
 
 import org.junit.Before;
 
@@ -80,15 +83,15 @@ public class VirusBreakendTest extends TertiaryStageTest<VirusBreakendOutput> {
 
     @Override
     protected List<String> expectedCommands() {
-        return List.of("export PATH=\"${PATH}:/opt/tools/gridss/2.13.2\"",
+        return List.of("export PATH=\"${PATH}:/opt/tools/gridss/" + VIRUSBREAKEND_GRIDSS.runVersion() + "\"",
                 "export PATH=\"${PATH}:/opt/tools/repeatmasker/4.1.1\"",
                 "export PATH=\"${PATH}:/opt/tools/kraken2/2.1.0\"",
                 "export PATH=\"${PATH}:/opt/tools/samtools/1.14\"",
                 "export PATH=\"${PATH}:/opt/tools/bcftools/1.9\"",
                 "export PATH=\"${PATH}:/opt/tools/bwa/0.7.17\"",
-                "/opt/tools/gridss/2.13.2/virusbreakend --output /data/output/tumor.virusbreakend.vcf " + "--workingdir /data/output "
+                "/opt/tools/gridss/" + VIRUSBREAKEND_GRIDSS.runVersion() + "/virusbreakend --output /data/output/tumor.virusbreakend.vcf " + "--workingdir /data/output "
                         + "--reference /opt/resources/reference_genome/37/Homo_sapiens.GRCh37.GATK.illumina.fasta "
-                        + "--db /opt/resources/virusbreakend_db --jar /opt/tools/gridss/2.13.2/gridss.jar --gridssargs \"--jvmheap 60G\" "
+                        + "--db /opt/resources/virusbreakend_db --jar /opt/tools/gridss/" + GRIDSS.runVersion() + "/gridss.jar --gridssargs \"--jvmheap 60G\" "
                         + "/data/input/tumor.bam");
     }
 }
