7dd7a0d44bed7ea6af6d6f1b44e0026d0e5630fe
Matthijs van Niekerk <m.vanniekerk@hartwigmedicalfoundation.nl>
2024-02-07 11:18:11 +0100

    INFRA-110 use noop publisher

--- cluster/src/main/java/com/hartwig/pipeline/aqua/AquaPublisher.java
+++ cluster/src/main/java/com/hartwig/pipeline/aqua/AquaPublisher.java
@@ -1,44 +1,75 @@
 package com.hartwig.pipeline.aqua;
 
 import java.io.IOException;
+import java.time.Instant;
 
+import com.hartwig.events.EventPublisher;
+import com.hartwig.events.aqua.MolecularPipelineCompletedEvent;
+import com.hartwig.events.aqua.MolecularPipelineStartedEvent;
 import com.hartwig.events.aqua.model.AquaEvent;
+import com.hartwig.events.pipeline.Pipeline;
+import com.hartwig.events.pubsub.NoopEventPublisher;
 import com.hartwig.events.pubsub.PubsubEventBuilder;
 import com.hartwig.pipeline.Arguments;
 import com.hartwig.pipeline.input.SomaticRunMetadata;
 import com.hartwig.pipeline.tools.VersionUtils;
 
-import org.jetbrains.annotations.NotNull;
+public class AquaPublisher {
+    private final EventPublisher<AquaEvent> publisher;
+    private final String barcode;
+    private final boolean isShallow;
+    private final boolean isTargeted;
+    private final Pipeline.Context pipelineContext;
+    private final String version;
 
-public interface AquaPublisher {
-    void publishStarted();
+    public AquaPublisher(final EventPublisher<AquaEvent> publisher, final String barcode, final boolean isShallow,
+            final boolean isTargeted, final Pipeline.Context pipelineContext, final String version) {
+        this.publisher = publisher;
+        this.barcode = barcode;
+        this.isShallow = isShallow;
+        this.isTargeted = isTargeted;
+        this.pipelineContext = pipelineContext;
+        this.version = version;
+    }
 
-    void publishComplete(String status);
-
-    static AquaPublisher create(Arguments arguments, SomaticRunMetadata somaticRunMetadata) throws IOException {
-        if (!arguments.publishToAqua()) {
-            return noopAquaPublisher();
-        }
+    public static AquaPublisher create(Arguments arguments, SomaticRunMetadata somaticRunMetadata) throws IOException {
         var project = arguments.aquaProject().orElseThrow();
         var barcode = somaticRunMetadata.barcode();
         var shallow = arguments.shallow();
         var targeted = arguments.useTargetRegions();
         var pipelineContext = arguments.context();
         var version = VersionUtils.pipelineVersion();
-        var publisher = new PubsubEventBuilder().newPublisher(project, new AquaEvent.EventDescriptor());
-        return new AquaPublisherImpl(publisher, barcode, shallow, targeted, pipelineContext, version);
+        EventPublisher<AquaEvent> publisher;
+        if (!arguments.publishToAqua()) {
+            publisher = new NoopEventPublisher<>();
+        } else {
+            publisher = new PubsubEventBuilder().newPublisher(project, new AquaEvent.EventDescriptor());
+        }
+        return new AquaPublisher(publisher, barcode, shallow, targeted, pipelineContext, version);
     }
 
-    @NotNull
-    private static AquaPublisher noopAquaPublisher() {
-        return new AquaPublisher() {
-            @Override
-            public void publishStarted() {
-            }
+    public void publishStarted() {
+        var startedEvent = MolecularPipelineStartedEvent.builder()
+                .timestamp(Instant.now())
+                .barcode(barcode)
+                .shallow(isShallow)
+                .targeted(isTargeted)
+                .pipelineContext(pipelineContext)
+                .version(version)
+                .build();
+        publisher.publish(startedEvent);
+    }
 
-            @Override
-            public void publishComplete(String status) {
-            }
-        };
+    public void publishComplete(String status) {
+        var completedEvent = MolecularPipelineCompletedEvent.builder()
+                .timestamp(Instant.now())
+                .barcode(barcode)
+                .shallow(isShallow)
+                .targeted(isTargeted)
+                .pipelineContext(pipelineContext)
+                .version(version)
+                .status(status)
+                .build();
+        publisher.publish(completedEvent);
     }
 }
