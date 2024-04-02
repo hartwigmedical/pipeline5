7a7b6410e29b4aa26c49683e9e7b1a700c4572d0
Ned Leitch <ned.leitch@gmail.com>
2024-04-02 11:45:47 -0400

    ACTIN-788 Fix CuppaTest

--- cluster/src/test/java/com/hartwig/pipeline/tertiary/cuppa/CuppaTest.java
+++ cluster/src/test/java/com/hartwig/pipeline/tertiary/cuppa/CuppaTest.java
@@ -99,7 +99,7 @@ public class CuppaTest extends TertiaryStageTest<CuppaOutput> {
                 + "-ref_genome_version V37 "
                 + "-sample_data_dir /data/input/results "
                 + "-output_dir /data/output",
-                "(source /opt/tools/pycuppa/2.1.0_venv/bin/activate && "
+                "(source /opt/tools/pycuppa/2.1.1_venv/bin/activate && "
                 + "python -m cuppa.predict "
                 + "--classifier_path /opt/resources/cuppa/37/cuppa_classifier.37.pickle.gz "
                 + "--features_path /data/output/tumor.cuppa_data.tsv.gz --output_dir /data/output --sample_id tumor "
