5684bf9796c8bae5d0cd16d2ac64139585b74024
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2024-04-11 10:05:37 +0200

    ACTIN-788: Bump ORANGE version

--- cluster/src/main/java/com/hartwig/pipeline/tools/HmfTool.java
+++ cluster/src/main/java/com/hartwig/pipeline/tools/HmfTool.java
@@ -1,8 +1,8 @@
 package com.hartwig.pipeline.tools;
 
-import com.hartwig.computeengine.execution.vm.VmDirectories;
-
 import static java.lang.String.format;
+
+import com.hartwig.computeengine.execution.vm.VmDirectories;
 
 public enum HmfTool {
 
@@ -18,7 +18,7 @@ public enum HmfTool {
     LILAC("1.6", 16, 24, 8, false),
     LINX("1.25", 8, 12, 4, false),
     MARK_DUPS("1.1.4", 40, 120, 24, false),
-    ORANGE("3.3.0", 16, 18, 4, false),
+    ORANGE("3.3.1", 16, 18, 4, false),
     PAVE("1.6", 30, 40, 8, false),
     PEACH("1.8", 1, 4, 2, false),
     PURPLE("4.0.2", 30, 40, 8, false),
