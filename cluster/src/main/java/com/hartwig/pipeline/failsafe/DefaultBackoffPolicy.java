56cc755af7885a31a54de3f6f005a4a690925c3a
Hong Wing Lee <h.lee@hartwigmedicalfoundation.nl>
2024-05-09 19:15:56 +1000

    INFRA-469: when a gcp request has failed, print the cause as well to make the problem easier to diagnose.

--- cluster/src/main/java/com/hartwig/pipeline/failsafe/DefaultBackoffPolicy.java
+++ cluster/src/main/java/com/hartwig/pipeline/failsafe/DefaultBackoffPolicy.java
@@ -20,9 +20,16 @@ public class DefaultBackoffPolicy<R> extends RetryPolicy<R> {
         abortOn(InvalidArgumentException.class);
         onAbort(e -> LOGGER.error("Unable to submit operation", e.getFailure()));
         handle(Exception.class);
-        onFailedAttempt(rExecutionAttemptedEvent -> LOGGER.warn("[{}] failed: {}",
-                taskName,
-                rExecutionAttemptedEvent.getLastFailure().getMessage()));
+        onFailedAttempt(rExecutionAttemptedEvent ->
+        {
+            // we need to keep tracing the cause to print out the real failure reason
+            Throwable lastFailure = rExecutionAttemptedEvent.getLastFailure();
+            while(lastFailure != null)
+            {
+                LOGGER.warn("[{}] failed: {}", taskName, lastFailure.getMessage());
+                lastFailure = lastFailure.getCause();
+            }
+        });
     }
 
     public static <R> DefaultBackoffPolicy<R> of(final String taskName) {
