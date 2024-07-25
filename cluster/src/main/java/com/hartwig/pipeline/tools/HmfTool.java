28ad2fff6fccfba785082b20599c085f243e93c5
Luan Nguyen <l.nguyen@hartwigmedicalfoundation.nl>
2024-07-25 12:09:34 +1000

    AUS-243: Removed gridss and gripss tool definition from HmfTool

--- cluster/src/main/java/com/hartwig/pipeline/tools/HmfTool.java
+++ cluster/src/main/java/com/hartwig/pipeline/tools/HmfTool.java
@@ -13,8 +13,6 @@ public enum HmfTool {
     COBALT("1.16", 20, 24, 16, false),
     CUPPA("2.1.1", Defaults.JAVA_HEAP, 16, 4, false),
     ESVEE("1.0.0rc", Defaults.JAVA_HEAP, 64, 24, true),
-    GRIDSS("2.13.3", Defaults.JAVA_HEAP, 64, 24, false),
-    GRIPSS("2.4", 16, 24, 4, false),
     HEALTH_CHECKER("3.5", Defaults.JAVA_HEAP, 32, 8, false),
     LILAC("1.6", 16, 24, 8, false),
     LINX("1.25", 8, 12, 4, false),
