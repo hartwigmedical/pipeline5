bc7c60c479643614bf3bd8ef5c0e625f88c762c9
Stef van Lieshout <stefvanlieshout@fastmail.fm>
2024-09-20 17:04:47 +0200

    INFRA-786 Add 31SNP output to genotype stage

--- cluster/src/main/java/com/hartwig/pipeline/resource/RefGenome38ResourceFiles.java
+++ cluster/src/main/java/com/hartwig/pipeline/resource/RefGenome38ResourceFiles.java
@@ -155,6 +155,9 @@ public class RefGenome38ResourceFiles implements ResourceFiles {
     }
 
     @Override
+    public String genotypeMipSnpsDB() { return formPath(GENOTYPE_SNPS, "31SNPtaq.vcf"); }
+
+    @Override
     public String driverGenePanel() {
         return formPath(GENE_PANEL, "DriverGenePanel.38.tsv");
     }
