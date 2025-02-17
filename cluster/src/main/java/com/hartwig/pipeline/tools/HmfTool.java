23df56c92e34ee4d827929da025c55c70826e970
Charles Shale <shalecharles@gmail.com>
2025-02-17 15:47:26 +1100

    AUS-243: updated tools to releases

--- cluster/src/main/java/com/hartwig/pipeline/tools/HmfTool.java
+++ cluster/src/main/java/com/hartwig/pipeline/tools/HmfTool.java
@@ -6,27 +6,27 @@ import com.hartwig.computeengine.execution.vm.VmDirectories;
 
 public enum HmfTool {
 
-    AMBER("4.1-rc.3", 24, 16, false), // 4.0.1 -> 4.1
-    BAM_TOOLS("1.3-rc.1", 24, 16, false), // 1.2.1 -> 1.3
-    CHORD("2.1-rc.1", 24, 4, false), // 2.02_1.14 ->
+    AMBER("4.1.1", 24, 16, false),
+    BAM_TOOLS("1.3", 24, 16, false),
+    CHORD("2.1.0", 24, 4, false),
     CIDER("1.0.3", 24, 4, false),
-    COBALT("2.0-rc.1", 24, 16, false), // 1.16 -> 2.0
-    CUPPA("2.3.0", 16, 4, false), // 2.1.1 -> 2.3
-    ESVEE("1.0-rc.8", 64, 32, false),
-    HEALTH_CHECKER("3.6-rc.1", 32, 8, false), // 3.5 -> 3.6
+    COBALT("2.0", 24, 16, false),
+    CUPPA("2.3.0", 16, 4, false),
+    ESVEE("1.0", 64, 32, false),
+    HEALTH_CHECKER("3.6", 32, 8, false),
     LILAC("1.6", 24, 16, false),
-    LINX("2.0-rc.1", 16, 4, false), // 1.25 -> 2.0
-    REDUX("1.1-rc.3", 64, 32, false), // MarkDups 1.1.7 -> Redux 1.0
-    ORANGE("3.7.1-rc.1", 16, 4, false), // 3.5.1 -> 3.7.1
-    PAVE("1.7-rc.1", 32, 8, false), // 1.6 -> 1.7
+    LINX("2.0", 16, 4, false),
+    REDUX("1.1", 64, 32, false),
+    ORANGE("3.7.1", 16, 4, false),
+    PAVE("1.7", 32, 8, false),
     PEACH("2.0.0", 4, 2, false, 50),
-    PURPLE("4.1-rc.3", 40, 8, false), // 4.0.2 -> 4.1
-    SAGE("4.0-rc.4", 64, 32, false), // 3.4.3 -> 4.0
+    PURPLE("4.1", 40, 8, false),
+    SAGE("4.0", 64, 32, false),
     SIGS("1.2.1", 16, 4, false, 30),
-    TEAL("1.3.3", 32, 32, false), // 1.2.2 -> 1.3.3
-    V_CHORD("1.0", 4, 2, false), // will be 1.0
+    TEAL("1.3.3", 32, 32, false),
+    V_CHORD("1.0", 4, 2, false),
     VIRUSBREAKEND_GRIDSS("2.13.3", 64, 8, false),
-    VIRUS_INTERPRETER("1.7-rc.1", 8, 2, false); // 1.3 -> 1.7
+    VIRUS_INTERPRETER("1.7", 8, 2, false);
 
     private static final String PILOT_VERSION = "pilot"; // will pick up the jar from /opt/toolName/pilot/toolName.jar
     private final String toolName;
