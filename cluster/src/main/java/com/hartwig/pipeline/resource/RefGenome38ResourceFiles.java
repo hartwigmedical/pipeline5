f496101d29af82b716bf3a1208b5cddfe69ebac3
David Koetsier <david.f.koetsier@gmail.com>
2025-05-30 16:35:21 +0200

    DEV-4181: Match 38 SNV PON name to name in repository

--- cluster/src/main/java/com/hartwig/pipeline/resource/RefGenome38ResourceFiles.java
+++ cluster/src/main/java/com/hartwig/pipeline/resource/RefGenome38ResourceFiles.java
@@ -85,7 +85,7 @@ public class RefGenome38ResourceFiles implements ResourceFiles {
         return formPath(SAGE, "clinvar.38.vcf.gz");
     }
     public String germlinePon() {
-        return formPath(SAGE, "SageGermlinePon.1000x.38.tsv.gz");
+        return formPath(SAGE, "hmf_wgs_sage_pon_1000.38.tsv.gz");
     }
     public String gnomadPonCache() {
         return formPath(GNOMAD, "");
